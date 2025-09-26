74090b1e4f67cc52a2f84a79ee0f305c068a9562
jyooni <berry3891@naver.com>
2025-09-26 15:48:09 +0900

    feat: gathering handler 기능 추가 - 사용자의 입력 값에 따라 출력값이 변경되도록 기능 추가

--- src/mocks/handlers/gathering.ts
+++ src/mocks/handlers/gathering.ts
@@ -1,30 +1,71 @@
-import {
-  mockGatheringCreate,
-  mockGatheringDelete,
-  mockGatheringDetail,
-  mockGatheringUpdate,
-} from "@/data/gathering";
+import { mockGatheringDelete, mockGatheringDetail } from "@/data/gathering";
+import { CreateGathering, UpdateGathering } from "@/types/gathering";
 import { http, HttpResponse } from "msw";
 
 const gatheringHandler = [
   // 모임 생성
-  http.post("http://localhost:4000/gathering/create", () => {
-    return HttpResponse.json(mockGatheringCreate);
-  }),
+  http.post(
+    "http://localhost:4000/gathering/create",
+    async ({ request, params }) => {
+      const id = Number(params.id);
+      const inputData = (await request.json()) as CreateGathering;
+
+      // inputData를 기반으로 모임 상세 정보 생성
+      const createdGathering = {
+        id: id,
+        title: inputData.title,
+        description: inputData.description,
+        category: inputData.category,
+        image: "",
+        createdAt: new Date().toISOString(),
+        currentMemberCount: 1,
+        maxMemberCount: inputData.maxMemberCount,
+        host: {
+          id: 1,
+          nickname: "YOON",
+          image: "",
+        },
+        condition: inputData.condition,
+        isJoined: false,
+      };
+
+      return HttpResponse.json(createdGathering);
+    }
+  ),
 
   // 모임 상세 조회
-  http.get("http://localhost:4000/gathering/detail/:id", () => {
-    return HttpResponse.json(mockGatheringDetail);
+  http.get("http://localhost:4000/gathering/detail/:id", ({ params }) => {
+    const id = Number(params.id);
+
+    return HttpResponse.json({ ...mockGatheringDetail, id });
   }),
 
   // 모임 수정
-  http.patch("http://localhost:4000/gathering/detail/:id", () => {
-    return HttpResponse.json(mockGatheringUpdate);
-  }),
+  http.patch(
+    "http://localhost:4000/gathering/detail/:id",
+    async ({ request, params }) => {
+      const id = Number(params.id);
+      const inputData = (await request.json()) as UpdateGathering;
+
+      // inputData를 기반으로 모임 상세 정보 수정
+      const updatedGathering = {
+        id: id,
+        title: inputData.title,
+        description: inputData.description,
+        category: inputData.category,
+        image: "",
+        maxMemberCount: inputData.maxMemberCount,
+        condition: inputData.condition,
+      };
+
+      return HttpResponse.json(updatedGathering);
+    }
+  ),
 
   // 모임 삭제
-  http.delete("http://localhost:4000/gathering/detail/:id", () => {
-    return HttpResponse.json(mockGatheringDelete);
+  http.delete("http://localhost:4000/gathering/detail/:id", ({ params }) => {
+    const id = Number(params.id);
+    return HttpResponse.json({ ...mockGatheringDelete, id });
   }),
 ];
 
