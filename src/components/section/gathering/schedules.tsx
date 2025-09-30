f3803bce69e205bd1aa7f66dd28a60776e35e9ad
jyooni <berry3891@naver.com>
2025-09-30 11:36:13 +0900

    refactor: main-content, side-bar, schedules 컴포넌트를 별도 파일로 분리

---
+++ src/components/section/gathering/schedules.tsx
@@ -0,0 +1,17 @@
+const Schedules = () => {
+  return (
+    <div className="space-y-4">
+      <div className="flex items-center justify-between">
+        <h2 className="text-xl font-bold">모임 일정</h2>
+        <button className="text-stone-500">더보기</button>
+      </div>
+
+      {/* Todo: Schedule Cards 추가하기 */}
+      <div className="flex h-[200px] w-full items-center justify-center bg-stone-50">
+        <p>Schedule Cards 추가하기</p>
+      </div>
+    </div>
+  );
+};
+
+export default Schedules;
---
+++ src/components/section/gathering/side-bar.tsx
@@ -0,0 +1,51 @@
+import { GetGatheringDetailResponse } from "@/types/gathering";
+
+const SideBar = ({ data }: { data: GetGatheringDetailResponse }) => {
+  return (
+    <div className="right-0 h-fit space-y-6 bg-stone-50 p-6">
+      {/* Details */}
+      <div className="bg-white p-6">
+        <h3 className="mb-4 text-lg font-bold">세부 정보</h3>
+        <div className="space-y-3">
+          {/* Created At */}
+          <div className="flex justify-between border-b border-zinc-100 pb-3">
+            <div className="text-sm text-stone-500">모임 생성일</div>
+            <div className="text-sm">
+              {new Date(data.createdAt).toLocaleDateString("ko-KR")}
+            </div>
+          </div>
+
+          {/* Join Condition: 일단은 '누구나 참여 가능' 하드코딩 */}
+          <div className="flex justify-between">
+            <div className="text-sm text-stone-500">참여 조건</div>
+            <div className="text-sm">누구나 참여 가능</div>
+          </div>
+        </div>
+      </div>
+
+      {/* Join Button */}
+      {/* Todo: isJoined 상태에 따른 스타일링 기능 구현 */}
+      <button className="w-full bg-zinc-800 py-3 font-bold text-white">
+        모임 참여하기
+      </button>
+
+      {/* Host Info */}
+      <div className="space-y-4">
+        <h3 className="text font-bold">모임장 정보</h3>
+        <div className="flex flex-col items-center space-y-2 text-center">
+          {/* Host Profile Image*/}
+          <div className="-full flex size-20 items-center justify-center bg-zinc-300">
+            <span className="text-sm text-stone-500">프로필</span>
+          </div>
+
+          {/* Host Nickname */}
+          <div>
+            <div className="text-center font-bold">{data.host.nickname}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SideBar;
