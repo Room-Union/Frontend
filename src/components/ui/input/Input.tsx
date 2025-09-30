a39a6684299088b3508772a0312f0f03f481292f
ziyoungsRoom <ilikeeeyo.o@gmail.com>
2025-09-30 15:40:24 +0900

    feat: ReactHookForm Input 컴포넌트 구현

---
+++ src/components/ui/input/Input.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { useFormContext } from "react-hook-form";
+
+interface InputProps {
+  name: string;
+  type?: string;
+  placeholder?: string;
+  className?: string;
+  label?: string;
+}
+
+const Input = ({
+  name,
+  type = "text",
+  placeholder,
+  className,
+  label,
+}: InputProps) => {
+  // 부모 컴포넌트 FormProvider애서 전달된 methods를 useFormContext 훅으로 사용
+  const { register } = useFormContext();
+
+  return (
+    <div className="flex flex-col gap-2">
+      {label && <label>{label}</label>}
+      <input
+        type={type}
+        placeholder={placeholder}
+        className={className}
+        {...register(name)}
+      />
+    </div>
+  );
+};
+
+export default Input;
