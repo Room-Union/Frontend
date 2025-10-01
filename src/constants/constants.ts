90a9ccbf5c9933b02910f33ccd6af4e8d0ba32e2
ziyoungsRoom <ilikeeeyo.o@gmail.com>
2025-10-01 14:40:59 +0900

    feat: 회원가입 Step 상수 추가

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -6,3 +6,10 @@ export const CATEGORIES = [
   { name: "정보·경제", value: "informationEconomy" },
   { name: "자기계발", value: "self-development" },
 ];
+
+export const SIGN_UP_STEPS = [
+  { id: 1, name: "이메일 입력" },
+  { id: 2, name: "이메일 인증" },
+  { id: 3, name: "비밀번호 입력" },
+  { id: 4, name: "프로필 입력" },
+];
