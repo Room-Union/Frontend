f228c19686c7e7f416a7310389fc3bf711f59655
정민석 <48466548+phantom2115@users.noreply.github.com>
2025-10-01 18:03:42 +0900

    Fix: 마이페이지 레이아웃 수정 및 파일 삭제 (#15)
    * remove: edit 페이지 삭제
    
    * fix: 마이페이지 레이아웃 수정

--- src/page/my-page.tsx
+++ src/page/my-page.tsx
@@ -2,11 +2,11 @@ import { Gatherings, UserProfile } from "@/components/section";
 
 const MyPage = () => {
   return (
-    <main className="flex min-h-screen w-screen flex-col gap-6 bg-white px-90">
+    <div className="flex flex-col gap-6 bg-white">
       <UserProfile />
       <Gatherings title="내가 생성한 모임" />
       <Gatherings title="내가 가입한 모임" />
-    </main>
+    </div>
   );
 };
 
