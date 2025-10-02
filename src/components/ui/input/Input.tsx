a7fc0b41cddd1f4dbc0ba9039094101b80fc1529
ziyoungsRoom <ilikeeeyo.o@gmail.com>
2025-10-02 14:45:15 +0900

    feat: Input 공통 컴포넌트 textarea 옵션 추가

--- src/components/ui/input/Input.tsx
+++ src/components/ui/input/Input.tsx
@@ -5,7 +5,7 @@ import StatusMessage from "./StatusMessage";
 
 interface InputProps {
   name: string;
-  type?: string;
+  type?: "text" | "password" | "textarea";
   placeholder?: string;
   className?: string;
   label?: string | React.ReactNode;
@@ -29,12 +29,25 @@ const Input = ({
   return (
     <div className="flex flex-col gap-2">
       {label && <label>{label}</label>}
-      <input
-        type={type}
-        placeholder={placeholder}
-        className={className}
-        {...register(name)}
-      />
+      {/* type이 'text' 또는 'password'일 경우 */}
+      {!(type === "textarea") && (
+        <input
+          type={type}
+          placeholder={placeholder}
+          className={className}
+          {...register(name)}
+        />
+      )}
+
+      {/* type이 'textarea'일 경우 */}
+      {type === "textarea" && (
+        <textarea
+          placeholder={placeholder}
+          className={className}
+          {...register(name)}
+        />
+      )}
+      {/* 유효성 검사 통과 메세지 입력 시 노출 */}
       {correctMessage && (
         <StatusMessage
           name={name}
