0857e876d70804743574c901020a84d2951ca913
정민석 <minskyson7@gmail.com>
2025-10-02 14:11:29 +0900

    chore: 소셜 아이콘 추가

---
+++ src/assets/icons/SocialX.tsx
@@ -0,0 +1,4 @@
+import * as React from "react";
+import type { SVGProps } from "react";
+const SvgSocialX = (props: SVGProps<SVGSVGElement>) => <svg fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25 24" className="inline-block align-middle fill-current" role="img" aria-hidden="true" {...props}><path d="M18.8263 1.90393H22.1998L14.8297 10.3274L23.5 21.7899H16.7112L11.394 14.838L5.30995 21.7899H1.93443L9.81743 12.78L1.5 1.90393H8.46111L13.2674 8.25826L18.8263 1.90393ZM17.6423 19.7707H19.5116L7.44539 3.81706H5.43946L17.6423 19.7707Z" fill="#9DA4AE" /></svg>;
+export default SvgSocialX;
