04f2866f6d8a40d53d24a4527a2ae1c4732abbc8
ziyoungsRoom <ilikeeeyo.o@gmail.com>
2025-10-02 12:46:31 +0900

    feat: checkbox/radio Input 공통 컴포넌트 구현

---
+++ src/components/ui/input/OptionInput.tsx
@@ -0,0 +1,57 @@
+import { useFormContext } from "react-hook-form";
+import StatusMessage from "./StatusMessage";
+
+interface OptionInputProps {
+  type?: "radio" | "checkbox";
+  name: string;
+  label?: string;
+  count?: number;
+  options: Record<"name" | "value", string>[];
+  className: string;
+  correctMessage?: string;
+}
+const OptionInput = ({
+  type = "checkbox",
+  name,
+  label,
+  options,
+  className,
+  correctMessage,
+}: OptionInputProps) => {
+  const {
+    register,
+    formState: { errors, dirtyFields },
+  } = useFormContext();
+
+  return (
+    <div className="flex flex-col gap-2">
+      {label && <label>{label}</label>}
+      <div className="grid grid-cols-3 gap-1">
+        {options.map((option, index) => {
+          return (
+            <label htmlFor={option.value} key={index} className={className}>
+              <input
+                id={option.value}
+                type={type}
+                value={option.value}
+                {...register(name)}
+                className="hidden"
+              ></input>
+              {option.name}
+            </label>
+          );
+        })}
+      </div>
+      {correctMessage && (
+        <StatusMessage
+          name={name}
+          errors={errors}
+          isDirty={dirtyFields}
+          correctMessage={correctMessage}
+        />
+      )}
+    </div>
+  );
+};
+
+export default OptionInput;
