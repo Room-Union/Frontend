682ae2265c59e1dc347b94bda0ff2404aebd3c05
송준혁 <83599564+J-Moment@users.noreply.github.com>
2025-10-03 07:42:34 +0900

    fix: svgr 설정 파일 수정 (#23)
    * fix: svgr 설정 파일 수정
    
    * remove: 중복되는 아이콘 제거
    
    * fix: svgr 설정 - stroke 속성 제거 및 currentColor 할당
    
    * rename: Svgr 케밥 케이스로 변경
    
    * chore: svgr 컴포넌트 변환 커맨드 수정
    
    * rename: svgr 케밥케이스로 변경

---
+++ src/assets/icons/meet-balls.tsx
@@ -0,0 +1,4 @@
+import * as React from "react";
+import type { SVGProps } from "react";
+const SvgMeetBalls = (props: SVGProps<SVGSVGElement>) => <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25 24" fill="currentColor" stroke="currentColor" className="inline-block align-middl" role="img" aria-hidden="true" {...props}><circle cx={10.8252} cy={20.7166} r={2.5} /><circle cx={20.8252} cy={20.7166} r={2.5} /><circle cx={30.8252} cy={20.7166} r={2.5} /></svg>;
+export default SvgMeetBalls;
