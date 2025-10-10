75bcced6260d6777ef607f9f81a9143e7238be76
JIYOON <112847202+jyooni99@users.noreply.github.com>
2025-10-10 11:50:18 +0900

    feat: file-input 구현 (#29)
    * feat: file-input 구현
    
    * 사용하지 않는 클래스명 제거
    
    * design: 회원가입 layout height 수정 (#27)
    
    * design: 회원가입 layout height 수정
    
    * design: step indicator top 지정으로 변경
    
    * chore: index 파일 추가
    
    * refactor: onUpload를 handleUpload로 네이밍 변경
    
    ---------
    
    Co-authored-by: Jiyoung <ilikeeeyo.o@gmail.com>

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,7 +1,17 @@
 import Button from "./button/button";
+import UploadButton from "./button/upload-button";
 import CategoryInput from "./input/category-input";
+import FileInput from "./input/file-input";
 import GenderInput from "./input/gender-input";
 import Input from "./input/Input";
 import Modal from "./modal/gathering-modal";
 
-export { Button, CategoryInput, GenderInput, Input, Modal };
+export {
+  Button,
+  CategoryInput,
+  FileInput,
+  GenderInput,
+  Input,
+  Modal,
+  UploadButton,
+};
