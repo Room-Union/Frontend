411e76b50c253edbbacc4c86f353a52fd3b163ae
ziyoungsRoom <ilikeeeyo.o@gmail.com>
2025-10-10 14:30:07 +0900

    feat: 회원가입 ReactHookForm 추가

--- src/page/sign-up-page.tsx
+++ src/page/sign-up-page.tsx
@@ -7,10 +7,24 @@ import {
   ProfileEntryStep,
   StepIndicator,
 } from "@/components/section";
+import { signUpSchema } from "@/validation/sign-up-validation";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { useState } from "react";
+import { FormProvider, useForm } from "react-hook-form";
 
 const SignUpPage = () => {
   const [step, setStep] = useState<number>(1);
+
+  const methods = useForm({
+    resolver: zodResolver(signUpSchema),
+    mode: "onChange",
+    defaultValues: {
+      gender: "female",
+      categories: [],
+    },
+  });
+
+  const handleSubmit = methods.handleSubmit;
 
   const moveToNextStep = () => {
     setStep((prev) => prev + 1);
@@ -20,14 +34,20 @@ const SignUpPage = () => {
     <div className="flex h-full flex-col items-center justify-center">
       <StepIndicator step={step} />
       {/* Progress bar 추가 예정 */}
-      <form className="flex flex-col">
-        {step === 1 && <EmailEntryStep moveToNextStep={moveToNextStep} />}
-        {step === 2 && (
-          <EmailVerificationStep moveToNextStep={moveToNextStep} />
-        )}
-        {step === 3 && <PasswordEntryStep moveToNextStep={moveToNextStep} />}
-        {step === 4 && <ProfileEntryStep />}
-      </form>
+
+      <FormProvider {...methods}>
+        <form
+          onSubmit={handleSubmit((data) => console.log(data))}
+          className="flex flex-col"
+        >
+          {step === 1 && <EmailEntryStep moveToNextStep={moveToNextStep} />}
+          {step === 2 && (
+            <EmailVerificationStep moveToNextStep={moveToNextStep} />
+          )}
+          {step === 3 && <PasswordEntryStep moveToNextStep={moveToNextStep} />}
+          {step === 4 && <ProfileEntryStep />}
+        </form>
+      </FormProvider>
     </div>
   );
 };
