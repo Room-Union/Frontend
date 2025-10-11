7baf010a4dc9dabd51f8eb402cb54f39d3de531e
jyooni <berry3891@naver.com>
2025-10-11 20:46:39 +0900

    fix: category 값 변경

--- src/data/gathering.ts
+++ src/data/gathering.ts
@@ -9,7 +9,7 @@ const mockGatheringCreate: CreateGatheringRequest = {
   name: "온라인 영화 토론 모임",
   description:
     "매주 다른 영화를 함께 보고 생각을 나누는 시간입니다. 다양한 장르의 영화를 통해 새로운 관점을 발견하고, 영화를 사랑하는 사람들과 깊이 있는 대화를 나눠보세요. 영화에 대한 사전 지식이 없어도 괜찮습니다. 함께 영화를 감상하고 자유롭게 의견을 나누는 것이 목적입니다.",
-  category: "문화・예술",
+  category: "CULTURE_ART",
   meetingImage: undefined,
   maxMemberCount: 30,
   platformUrls: ["https://discord.gg/abce"],
@@ -20,7 +20,7 @@ const mockGetGatheringDetail: GetGatheringDetailResponse = {
   name: "온라인 영화 토론 모임",
   description:
     "매주 다른 영화를 함께 보고 생각을 나누는 시간입니다. 다양한 장르의 영화를 통해 새로운 관점을 발견하고, 영화를 사랑하는 사람들과 깊이 있는 대화를 나눠보세요. 영화에 대한 사전 지식이 없어도 괜찮습니다. 함께 영화를 감상하고 자유롭게 의견을 나누는 것이 목적입니다.",
-  category: "문화・예술",
+  category: "CULTURE_ART",
   meetingImage: "",
   createdAt: "2025-09-26T10:30:00.000Z",
   currentMemberCount: 24,
@@ -36,7 +36,7 @@ const mockUpdateGathering: UpdateGatheringRequest = {
   name: "온라인 게임 모임",
   description:
     "매주 다른 게임을 함께 하는 시간입니다. 다양한 장르의 게임을 즐기는 것이 목적입니다.",
-  category: "문화・예술",
+  category: "CULTURE_ART",
   meetingImage: undefined,
   maxMemberCount: 50,
 };
