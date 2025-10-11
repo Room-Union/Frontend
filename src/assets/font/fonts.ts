ecf56398097bf6a3433ac5dfd453c0282718664c
송준혁 <83599564+J-Moment@users.noreply.github.com>
2025-10-11 20:37:46 +0900

    feat: 폰트 추가 (#36)

---
+++ src/assets/font/fonts.ts
@@ -0,0 +1,8 @@
+import localFont from "next/font/local";
+
+export const pretendard = localFont({
+    src: "./PretendardVariable.woff2",
+    weight: "100 900",
+    style: "normal",
+    variable: "--font-pretendard",
+});
