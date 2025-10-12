258b621dc07607fbc29724a2aaacc7f51abf155a
SongJunHyuk <83599564+J-Moment@users.noreply.github.com>
2025-10-12 18:06:22 +0900

    refactor: 아이콘 설정 변경
    1. 아이콘 viewBox 재설정
    2. svg-colored, icon-colored 디렉토리 생성
    3. 새로운 아이콘 업데이트

--- src/assets/icons/plus.tsx
+++ src/assets/icons/plus.tsx
@@ -1,4 +1,4 @@
 import * as React from "react";
 import type { SVGProps } from "react";
-const SvgPlus = (props: SVGProps<SVGSVGElement>) => <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25 24" fill="currentColor" stroke="currentColor" className="inline-block align-middle" role="img" aria-hidden="true" {...props}><path d="M4.17578 12H20.1758" strokeWidth={2} strokeLinecap="round" /><path d="M12.1758 20L12.1758 4" strokeWidth={2} strokeLinecap="round" /></svg>;
+const SvgPlus = (props: SVGProps<SVGSVGElement>) => <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25 24" className="inline-block align-middle" role="img" aria-hidden="true" fill="currentColor" stroke="currentColor" {...props}><path d="M4.17578 12H20.1758" strokeWidth={2} strokeLinecap="round" /><path d="M12.1758 20L12.1758 4" strokeWidth={2} strokeLinecap="round" /></svg>;
 export default SvgPlus;
