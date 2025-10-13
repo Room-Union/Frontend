a7bc26c67f0627fc2214220e6d9f7fdbdeade579
ziyoungsRoom <ilikeeeyo.o@gmail.com>
2025-10-13 17:09:47 +0900

    feat: Indicator step index 수정

--- src/page/sign-up-page.tsx
+++ src/page/sign-up-page.tsx
@@ -54,7 +54,7 @@ const SignUpPage = () => {
 
   return (
     <div className="flex h-full flex-col items-center justify-center">
-      <StepIndicator step={currentStepIndex} />
+      <StepIndicator step={currentStepIndex + 1} />
       {/* Progress bar 추가 예정 */}
 
       <FormProvider {...methods}>
