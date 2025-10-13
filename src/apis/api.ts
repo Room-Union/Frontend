cdd85c41ba1d331b760060f6bbef06b9128d266c
jyooni <berry3891@naver.com>
2025-10-13 18:31:36 +0900

    feat: 토큰 하드코딩으로 추가

--- src/apis/api.ts
+++ src/apis/api.ts
@@ -16,6 +16,10 @@ api.interceptors.request.use(
     const token = getAccessToken();
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
+    } else {
+      // 아직 getAccessToken 함수로 토큰을 받아오질 못해서, 하드 코딩으로 바꿔가시면서 테스트 해야합니다...
+      config.headers.Authorization =
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxMSIsImVtYWlsIjoiYmVycnkzODkxQGdtYWlsLmNvbSIsImlhdCI6MTc2MDM0NDI2NCwiZXhwIjoxNzYwOTQ5MDY0fQ.gfM8Tcow4ICHZtZH6FpHUEwuobWcvhMeMErEVpwowlc";
     }
     return config;
   },
