1c783a91164ad551d21b6cdd4b512d8623c1011f
ziyoungsRoom <ilikeeeyo.o@gmail.com>
2025-10-13 19:53:27 +0900

    feat: api 요청 data 전달 형식 수정

--- src/apis/auth/auth.api.ts
+++ src/apis/auth/auth.api.ts
@@ -2,7 +2,5 @@ import { SignUpRequest } from "@/types/user";
 import { api } from "../api";
 
 export const signUpUser = async (data: SignUpRequest) => {
-  await api.post("/users/signup", {
-    body: data,
-  });
+  await api.post("/users/sign-up", data);
 };
