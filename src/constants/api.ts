dbaeee1745ffb725397657aaa63fbc68e6cc4f24
jyooni <berry3891@naver.com>
2025-10-13 21:54:39 +0900

    chore: .env 파일 추가

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,3 +1,3 @@
-const TEST_BASE_URL = "https://localhost:4000";
-const BASE_URL = "http://43.201.140.54:8080/api/v1";
+const TEST_BASE_URL = process.env.NEXT_PUBLIC_TEST_BASE_URL;
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 export { BASE_URL, TEST_BASE_URL };
