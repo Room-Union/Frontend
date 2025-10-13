f31f02f67735ee281fc2cb8ab6fdc0c9a510a986
jyooni <berry3891@naver.com>
2025-10-13 21:38:28 +0900

    refactor: API 레이어에 있던 try-catch를 상위 레이어로 이동

--- src/apis/gathering/gathering.api.ts
+++ src/apis/gathering/gathering.api.ts
@@ -5,35 +5,24 @@ import {
 } from "@/types/gathering";
 
 const createGathering = async (data: GatheringFormData) => {
-  try {
-    const { meetingImage, ...formData } = data;
+  const { meetingImage, ...formData } = data;
 
-    const form = new FormData();
-    form.append("request", JSON.stringify(formData));
+  const form = new FormData();
+  form.append("request", JSON.stringify(formData));
 
-    // meetingImage가 있을 때에만
-    if (meetingImage) {
-      form.append("image", meetingImage);
-    }
+  // meetingImage가 있을 때에만
+  if (meetingImage) {
+    form.append("image", meetingImage);
 
     const response = await api.post(`/meetings`, form);
-
     return response.data;
-  } catch (error) {
-    console.error(error);
-    throw error;
   }
 };
 
 const getGatheringDetail = async (meetingId: GetGatheringDetailRequest) => {
-  try {
-    const response = await api.get(`/meetings/${meetingId}`);
+  const response = await api.get(`/meetings/${meetingId}`);
 
-    return response.data;
-  } catch (error) {
-    console.error(error);
-    throw error;
-  }
+  return response.data;
 };
 
 export { createGathering, getGatheringDetail };
