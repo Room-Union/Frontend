ef2839813604e599a1b5f1d0db4e045c33abda61
ziyoungsRoom <ilikeeeyo.o@gmail.com>
2025-10-14 15:50:00 +0900

    design: 로그인 layout 수정

--- src/app/(root)/(auth)/sign-in/layout.tsx
+++ src/app/(root)/(auth)/sign-in/layout.tsx
@@ -6,9 +6,11 @@ const Layout = ({
   children: React.ReactNode;
 }>) => {
   return (
-    <div>
+    <div className="h-screen bg-neutral-50">
       <Header className="absolute top-0 left-0 w-full" />
-      <main className="mx-auto max-w-[1065px]">{children}</main>
+      <main className="mx-auto flex h-full max-w-[1065px] items-center justify-center pt-20 pb-25">
+        {children}
+      </main>
       <Footer className="absolute bottom-0 left-0 w-full" />
     </div>
   );
