a5436e470d7ffaca58b2acf92449f014bb6a73e1
정민석 <minskyson7@gmail.com>
2025-10-14 20:55:05 +0900

    fix: index 파일 누락 부분 해결

--- src/components/section/index.ts
+++ src/components/section/index.ts
@@ -6,14 +6,18 @@ import EmailVerificationStep from "@/components/section/sign-up/email-verificati
 import PasswordEntryStep from "@/components/section/sign-up/password-entry-step";
 import ProfileEntryStep from "@/components/section/sign-up/profile-entry-step";
 import StepIndicator from "@/components/section/sign-up/step-indicator";
+import Gatherings from "@/components/section/user/gatherings";
+import UserProfile from "@/components/section/user/user-profile";
 
 export {
   EmailEntryStep,
   EmailVerificationStep,
+  Gatherings,
   MainContent,
   PasswordEntryStep,
   ProfileEntryStep,
   Schedules,
   SideBar,
   StepIndicator,
+  UserProfile,
 };
