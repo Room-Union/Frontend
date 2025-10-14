4d7d81fc43cca6060fa6aa64b133d6a229251e3b
정민석 <minskyson7@gmail.com>
2025-10-14 11:27:50 +0900

    design: 헤더 레이아웃 반응형 적용

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,4 +1,5 @@
 "use client";
+import SvgLogo from "@/assets/icons-colored/logo";
 import { cn } from "@/utils/cn";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
@@ -15,25 +16,26 @@ const Header = ({ className }: HeaderProps) => {
   return (
     <header
       className={cn(
-        "border-gray-neutral-100 flex h-20 items-center border-x-0 border-t-0 border-b bg-white",
+        "border-gray-neutral-100 pc:px-90 tb:px-6 mo:px-4 tb:h-20 mo:h-13 flex h-20 items-center border-x-0 border-t-0 border-b bg-white",
         className
       )}
     >
-      <div className="mx-auto flex w-full max-w-[1200px] items-center justify-between px-2">
+      <div className="flex w-full items-center justify-between">
         <div className="flex items-center gap-4">
-          <Link href="/" className="font-bold">
-            집콕
+          <Link href="/">
+            <SvgLogo className="tb:size-12 mo:size-9" />
           </Link>
           <Link
             href="/gathering/list/all"
             className={cn(
-              "p-4 text-base leading-6 font-medium tracking-[-0.32px]",
+              "mo:hidden tb:block p-4 text-base leading-6 font-medium tracking-[-0.32px]",
               isListPage ? "text-blue-600" : "text-gray-neutral-400"
             )}
           >
             모임 리스트
           </Link>
         </div>
+
         {isLogined ? (
           <Link href="/sign-in">유저 아이콘</Link>
         ) : (
