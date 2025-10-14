0a6bfaa7cf83dac258d5c889bd6645bdc87999f7
ziyoungsRoom <ilikeeeyo.o@gmail.com>
2025-10-14 15:54:12 +0900

    feat: 로그인 페이지 구현

--- src/page/sign-in-page.tsx
+++ src/page/sign-in-page.tsx
@@ -1,5 +1,31 @@
+"use client";
+
+import SignInForm from "@/components/section/auth/sign-in/sign-in-form";
+import FormContainer from "@/components/ui/form-container/form-container";
+import FormFooter from "@/components/ui/form-container/form-footer";
+import FormHeader from "@/components/ui/form-container/form-header";
+import Link from "next/link";
+import { FormProvider, useForm } from "react-hook-form";
+
 const SignInPage = () => {
-  return <div>SignInPage</div>;
+  const methods = useForm();
+  return (
+    <FormContainer>
+      <FormHeader />
+      <FormProvider {...methods}>
+        <form className="w-full">
+          <SignInForm />
+          <FormFooter />
+          <div className="mt-[30px] flex justify-center">
+            집콕이 처음이신가요?
+            <Link href="/sign-up" className="ml-2 underline">
+              회원가입
+            </Link>
+          </div>
+        </form>
+      </FormProvider>
+    </FormContainer>
+  );
 };
 
 export default SignInPage;
