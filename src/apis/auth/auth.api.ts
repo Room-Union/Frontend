7f81b3dda9296837cbc2a7f31f838e82ce2c8475
ziyoungsRoom <ilikeeeyo.o@gmail.com>
2025-10-15 01:04:40 +0900

    feat: 로그인 api 요청 작성

--- src/apis/auth/auth.api.ts
+++ src/apis/auth/auth.api.ts
@@ -1,6 +1,17 @@
-import { SignUpRequest } from "@/types/user";
-import { api } from "../api";
+import { SignInRequest, SignUpRequest } from "@/types/auth";
+import { api, api_v2 } from "../api";
 
-export const signUpUser = async (data: SignUpRequest) => {
+const signUpUser = async (data: SignUpRequest) => {
   await api.post("/users/sign-up", data);
 };
+
+const signInUser = async (data: SignInRequest) => {
+  try {
+    const response = await api_v2.post("/auth/login", data);
+    return response;
+  } catch (error) {
+    throw new Error(`로그인 실패 : ${error}`);
+  }
+};
+
+export { signInUser, signUpUser };
