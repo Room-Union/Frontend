a6f21c8430af1fab707cfec2d76f944a6191327d
ziyoungsRoom <ilikeeeyo.o@gmail.com>
2025-10-14 17:24:57 +0900

    feat: formFooter에 isDisabled props 추가

--- src/components/ui/form-container/form-footer.tsx
+++ src/components/ui/form-container/form-footer.tsx
@@ -1,9 +1,13 @@
 import Button from "../button/button";
 
-const FormFooter = () => {
+interface FormFooterProps {
+  isDisabled?: boolean;
+}
+
+const FormFooter = ({ isDisabled = false }: FormFooterProps) => {
   return (
     <div className="mt-[1.5rem] flex w-full justify-center">
-      <Button type="submit" variant={"primary"}>
+      <Button type="submit" disabled={isDisabled} variant={"primary"}>
         로그인
       </Button>
     </div>
