bf3c51dc60c0b405a21ede24053e09ecbfafc371
JIYOON <112847202+jyooni99@users.noreply.github.com>
2025-10-14 18:13:36 +0900

    refactor: gathering detail 리팩토링 (#53)
    * refactor: 모든 섹션이 동일한 추상화 레벨 유지
    
    * move: section/gathering/detail 폴더 추가 및 이동
    
    * rename: 파일명 변경
    
    * design: 헤더 배너 스타일 추가
    
    * fix: 레거시 속성 제거
    
    * feat: priority 속성 추가
    
    * style: text 스타일 추가

---
+++ src/components/section/gathering/detail/members.tsx
@@ -0,0 +1,9 @@
+const Members = () => {
+  return (
+    <>
+      <div className="flex h-[42px] bg-stone-50" />
+    </>
+  );
+};
+
+export default Members;
---
+++ src/components/ui/card/meet-up-card.tsx
@@ -0,0 +1,11 @@
+const MeetUpCard = () => {
+  return (
+    <div className="tb:h-[170px] tb:w-[340px] tb:p-5 border-gray-neutral-300 h-[138px] w-[282px] rounded-[20px] border p-[14px]">
+      <div className="flex w-full items-center justify-between">
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default MeetUpCard;
