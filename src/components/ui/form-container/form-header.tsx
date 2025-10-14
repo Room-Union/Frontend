dfda3e47ddf007f3b81995c2ff097609974c004d
ziyoungsRoom <ilikeeeyo.o@gmail.com>
2025-10-14 15:52:38 +0900

    design: 모달 모양의 폼 컴포넌트 구조화

---
+++ src/components/ui/form-container/form-header.tsx
@@ -0,0 +1,11 @@
+import { Logo } from "@/assets/icons-colored";
+
+const FormHeader = () => {
+  return (
+    <div>
+      <Logo width={"88px"} height={"88px"} />
+    </div>
+  );
+};
+
+export default FormHeader;
