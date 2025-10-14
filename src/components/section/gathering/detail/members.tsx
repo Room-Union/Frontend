2ae46b4ef9b98c0f3e01984b4331337fbbdfbf03
jyooni <berry3891@naver.com>
2025-10-14 13:28:46 +0900

    chore: 주석 추가

--- src/components/section/gathering/detail/members.tsx
+++ src/components/section/gathering/detail/members.tsx
@@ -1,7 +1,13 @@
+import MemberCard from "@/components/ui/card/member-card";
+
 const Members = () => {
   return (
     <>
-      <div className="flex h-[42px] bg-stone-50" />
+      {/* Todo: 멤버가 3명 이상일 때부터 더보기 버튼 추가 -> 더보기 누르면 모달 등장 */}
+      <div className="flex h-[42px] items-center">
+        <MemberCard size="sm" nickname="test" gender="MALE" isLeader />
+        <MemberCard size="lg" nickname="test2" gender="FEMALE" isLeader />
+      </div>
     </>
   );
 };
