8550b94b4357859cd6ed2a63f4ddbb1e71bc942b
ziyoungsRoom <ilikeeeyo.o@gmail.com>
2025-10-14 15:06:34 +0900

    rename: sign-in 폴더 section 내 auth 폴더 이동

--- src/components/section/index.ts
+++ src/components/section/index.ts
@@ -1,11 +1,11 @@
+import EmailEntryStep from "@/components/section/auth/sign-up/email-entry-step";
+import EmailVerificationStep from "@/components/section/auth/sign-up/email-verification-step";
+import PasswordEntryStep from "@/components/section/auth/sign-up/password-entry-step";
+import ProfileEntryStep from "@/components/section/auth/sign-up/profile-entry-step";
+import StepIndicator from "@/components/section/auth/sign-up/step-indicator";
 import MainContent from "@/components/section/gathering/main-content";
 import Schedules from "@/components/section/gathering/schedules";
 import SideBar from "@/components/section/gathering/side-bar";
-import EmailEntryStep from "@/components/section/sign-up/email-entry-step";
-import EmailVerificationStep from "@/components/section/sign-up/email-verification-step";
-import PasswordEntryStep from "@/components/section/sign-up/password-entry-step";
-import ProfileEntryStep from "@/components/section/sign-up/profile-entry-step";
-import StepIndicator from "@/components/section/sign-up/step-indicator";
 import Gatherings from "@/components/section/user/gatherings";
 import UserProfile from "@/components/section/user/user-profile";
 
