301fe62206e7375721027b22de1862b57b40de0f
ziyoungsRoom <ilikeeeyo.o@gmail.com>
2025-10-15 11:11:03 +0900

    feat: error 분기 처리 및 message 커스터마이징

--- src/apis/auth/auth.api.ts
+++ src/apis/auth/auth.api.ts
@@ -1,4 +1,5 @@
 import { SignInRequest, SignUpRequest } from "@/types/auth";
+import axios from "axios";
 import { api, api_v2 } from "../api";
 
 const signUpUser = async (data: SignUpRequest) => {
@@ -10,7 +11,23 @@ const signInUser = async (data: SignInRequest) => {
     const response = await api_v2.post("/auth/login", data);
     return response;
   } catch (error) {
-    throw new Error(`로그인 실패 : ${error}`);
+    // error code에 따른 분기 처리 & message 커스터마이징
+    if (axios.isAxiosError(error)) {
+      const status = error.response?.status;
+
+      switch (status) {
+        case 400:
+          throw new Error("아이디 또는 비밀번호가 올바르지 않습니다.");
+        case 500:
+          throw new Error(
+            "서버 오류가 발생했습니다. 잠시 후 다시 시도해주세요."
+          );
+        default:
+          throw new Error("오류가 발생했습니다. 다시 시도해주세요.");
+      }
+    } else {
+      throw new Error(`로그인 실패 : ${error}`);
+    }
   }
 };
 
