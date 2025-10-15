46e48ca6bb242d1cbe8de6e6b64a7253277e0200
jyooni <berry3891@naver.com>
2025-10-15 17:33:39 +0900

    feat: 컴포넌트 적용

--- src/components/section/gathering/detail/members.tsx
+++ src/components/section/gathering/detail/members.tsx
@@ -5,8 +5,8 @@ const Members = () => {
     <>
       {/* Todo: 멤버가 3명 이상일 때부터 더보기 버튼 추가 -> 더보기 누르면 모달 등장 */}
       <div className="flex h-[42px] items-center">
-        <MemberCard size="sm" nickname="test" gender="MALE" isLeader />
-        <MemberCard size="lg" nickname="test2" gender="FEMALE" isLeader />
+        <MemberCard nickname="test" gender="MALE" isLeader />
+        <MemberCard nickname="test2" gender="FEMALE" isLeader />
       </div>
     </>
   );
