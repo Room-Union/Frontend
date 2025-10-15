de1a994c20e1421b3167ecc812d31445272c9cb2
정민석 <minskyson7@gmail.com>
2025-10-15 14:01:59 +0900

    style: max width 태블릿 모바일에서 삭제

--- src/app/(root)/(main)/layout.tsx
+++ src/app/(root)/(main)/layout.tsx
@@ -9,7 +9,7 @@ const Layout = ({
     <div>
       <div className="min-h-screen">
         <Header />
-        <main className="pc:max-w-[1200px] tb:max-w-[744px] mo:max-w-[375px] pc:px-5 tb:px-6 mo:px-5 mx-auto">
+        <main className="pc:max-w-[1200px] pc:px-5 tb:px-6 mo:px-5 mx-auto">
           {children}
         </main>
       </div>
