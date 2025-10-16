94e3d56472d9445571a5201a8b0df005d0c42776
ziyoungsRoom <ilikeeeyo.o@gmail.com>
2025-10-16 18:27:19 +0900

    feat: 로그인 placeholder 추가

--- src/components/section/auth/sign-in/sign-in-form.tsx
+++ src/components/section/auth/sign-in/sign-in-form.tsx
@@ -10,11 +10,13 @@ const SignInForm = () => {
         <Input
           name="email"
           label="이메일"
+          placeholder="이메일을 입력하세요"
           className="w-full rounded-[12px] border border-neutral-50 bg-neutral-50 p-[12px] outline-none"
         />
         <Input
           name="password"
           label="비밀번호"
+          placeholder="비밀번호를 입력하세요"
           className="w-full rounded-[12px] border border-neutral-50 bg-neutral-50 p-[12px] outline-none"
         />
       </div>
