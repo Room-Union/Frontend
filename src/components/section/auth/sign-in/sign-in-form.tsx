969796b6c9c526553140bca8908fbfa3cb263911
ziyoungsRoom <ilikeeeyo.o@gmail.com>
2025-10-16 18:56:58 +0900

    design: input 스타일 수정

--- src/components/section/auth/sign-in/sign-in-form.tsx
+++ src/components/section/auth/sign-in/sign-in-form.tsx
@@ -1,5 +1,6 @@
 import { Input, LinkButton } from "@/components/ui";
 import FormFooter from "@/components/ui/form-container/form-footer";
+import { inputVariants } from "@/components/ui/input/input";
 import { useFormButtonDisabled } from "@/hooks";
 
 const SignInForm = () => {
@@ -11,14 +12,14 @@ const SignInForm = () => {
           name="email"
           label="이메일"
           placeholder="이메일을 입력하세요"
-          className="w-full rounded-[12px] border border-neutral-50 bg-neutral-50 p-[12px] outline-none"
+          className={`${inputVariants.input.tb_lg}`}
         />
         <Input
           name="password"
           type="password"
           label="비밀번호"
           placeholder="비밀번호를 입력하세요"
-          className="w-full rounded-[12px] border border-neutral-50 bg-neutral-50 p-[12px] outline-none"
+          className={`${inputVariants.input.tb_lg}`}
         />
       </div>
       <div className="tb:gap-[30px] flex w-full flex-col gap-[16px]">
