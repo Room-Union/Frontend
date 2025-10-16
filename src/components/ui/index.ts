3c16bcf58a1e5cceb36b1594c38ab7cd52f37113
정민석 <minskyson7@gmail.com>
2025-10-16 13:48:48 +0900

    feat: index 파일 추가

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,37 +1,30 @@
+import Banner from "./banner/banner";
+import AuthStatusButton from "./button/auth-status-button";
 import Button from "./button/button";
 import CarouselButton from "./button/carousel-button";
+import GnbTabButton from "./button/gnb-tab-button";
+import HamburgerMenuButton from "./button/hamburger-menu-button";
 import SeeMoreButton from "./button/see-more-button";
 import UploadButton from "./button/upload-button";
-
 import GatheringCard from "./card/gathering-card";
 import MeetUpCard from "./card/meet-up-card";
 import MemberCard from "./card/member-card";
-
 import Carousel from "./carousel/carousel";
-
 import MemberCount from "./count/member-count";
-
-import Progress from "./progress/progress";
-
-import Banner from "./banner/banner";
-
 import CategoryInput from "./input/category-input";
 import FileInput from "./input/file-input";
 import GenderInput from "./input/gender-input";
 import Input from "./input/Input";
-
+import AlertModal from "./modal/alert/alert-modal";
 import GatheringModal from "./modal/gathering/gathering-modal";
 import ModalNav from "./modal/modal-nav";
 import ModalWrapper from "./modal/modal-wrapper";
 import PasswordEditModal from "./modal/user/password-edit/password-edit-modal";
 import ProfileEditModal from "./modal/user/profile-edit/profile-edit-modal";
-
 import Profile from "./profile/profile";
-
-import AuthStatusButton from "./button/auth-status-button";
-import GnbTabButton from "./button/gnb-tab-button";
-import HamburgerMenuButton from "./button/hamburger-menu-button";
+import Progress from "./progress/progress";
 export {
+  AlertModal,
   AuthStatusButton,
   Banner,
   Button,
