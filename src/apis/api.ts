1ede784a9661c47e86c1d53caaea6acaa4b837f5
JIYOON <112847202+jyooni99@users.noreply.github.com>
2025-10-16 16:29:43 +0900

    style: 불필요한 else문 제거 및 외부 이미지 호스트 추가 (#70)
    * style: 불필요한 else문 제거
    
    * chore: 이미지 외부 호스트 업데이트

--- src/apis/api.ts
+++ src/apis/api.ts
@@ -1,4 +1,4 @@
-import { BASE_URL, TEST_BASE_URL, TEST_TOKEN } from "@/constants/api";
+import { BASE_URL, TEST_BASE_URL } from "@/constants/api";
 import { getAccessToken } from "@/utils/auth";
 import axios from "axios";
 
@@ -22,9 +22,6 @@ api.interceptors.request.use(
     const token = getAccessToken();
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
-    } else {
-      // 아직 getAccessToken 함수로 토큰을 받아오질 못해서, 하드 코딩으로 바꿔가시면서 테스트 해야합니다.
-      config.headers.Authorization = `Bearer ${TEST_TOKEN}`;
     }
     return config;
   },
