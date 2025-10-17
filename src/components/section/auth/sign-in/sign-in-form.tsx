5bd1fd42685b10922f712f70ad559b6c7f35aaaa
Jiyoung <ilikeeeyo.o@gmail.com>
2025-10-17 10:20:13 +0900

    design: 로그인 페이지 UI 작업 (#73)
    * fix: 링크 버튼 SignInForm 안으로 이동
    
    * fix: 링크 import 문 이동
    
    * design: 반응형에 따른 size 변경
    
    * design: 링크 버튼 폰트 속성 추가
    
    * rename: seeMoreButton -> linkButton 으로 네이밍 변경
    
    * fix: 네이밍 변경에 따른 추가 수정
    
    * feat: 링크 버튼 재사용을 위한 코드 수정
    
    * feat: LinkButton 로그인 페이지 추가
    
    * design: FormContainer 반응형 사이즈로 변경
    
    * feat: 로그인 placeholder 추가
    
    * feat: 비밀번호 input 타입 변경
    
    * design: input 스타일 수정

--- src/components/section/auth/sign-in/sign-in-form.tsx
+++ src/components/section/auth/sign-in/sign-in-form.tsx
@@ -1,25 +1,35 @@
-import { Input } from "@/components/ui";
+import { Input, LinkButton } from "@/components/ui";
 import FormFooter from "@/components/ui/form-container/form-footer";
+import { inputVariants } from "@/components/ui/input/input";
 import { useFormButtonDisabled } from "@/hooks";
 
 const SignInForm = () => {
   const { isDisabled } = useFormButtonDisabled(["email", "password"]);
   return (
-    <>
+    <div className="flex w-full flex-col gap-[24px]">
       <div className="flex w-full flex-col gap-[20px]">
         <Input
           name="email"
           label="이메일"
-          className="w-full rounded-[12px] border border-neutral-50 bg-neutral-50 p-[12px] outline-none"
+          placeholder="이메일을 입력하세요"
+          className={`${inputVariants.input.tb_lg}`}
         />
         <Input
           name="password"
+          type="password"
           label="비밀번호"
-          className="w-full rounded-[12px] border border-neutral-50 bg-neutral-50 p-[12px] outline-none"
+          placeholder="비밀번호를 입력하세요"
+          className={`${inputVariants.input.tb_lg}`}
         />
       </div>
-      <FormFooter isDisabled={isDisabled} />
-    </>
+      <div className="tb:gap-[30px] flex w-full flex-col gap-[16px]">
+        <FormFooter isDisabled={isDisabled} />
+        <div className="tb:typo-ui-sm-medium text-gray-neutral-500 typo-ui-xs-medium flex justify-center gap-[4px]">
+          집콕이 처음이신가요?
+          <LinkButton href="/sign-up" text="회원가입" />
+        </div>
+      </div>
+    </div>
   );
 };
 
