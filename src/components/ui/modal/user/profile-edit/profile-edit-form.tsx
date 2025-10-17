7c7701a7b614622cac61175c7b85e59b1bc9c6ff
정민석 <minskyson7@gmail.com>
2025-10-18 02:51:14 +0900

    feat: 프로필 수정 api 연동

--- src/components/ui/modal/user/profile-edit/profile-edit-form.tsx
+++ src/components/ui/modal/user/profile-edit/profile-edit-form.tsx
@@ -1,48 +1,100 @@
 "use client";
 
-import { Edit } from "@/assets/icons";
-import SvgProfileF1 from "@/assets/icons-colored/profile-f1";
-import { Input } from "@/components/ui";
+import useEditUserInfo from "@/apis/user/mutation/use-edit-user-info";
+import useGetUserInfo from "@/apis/user/query/use-get-user-info";
+import { Input, Profile } from "@/components/ui";
 import CategoryInput from "@/components/ui/input/category-input";
-import FileInput from "@/components/ui/input/file-input";
 import GenderInput from "@/components/ui/input/gender-input";
+import { editInfoSchema } from "@/validation/edit-info-validation";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRef, useState } from "react";
 import { FormProvider, useForm } from "react-hook-form"; // 추가
 import ModalNav from "../../modal-nav";
+import EditButton from "./profile-edit-button";
 
 interface ProfileEditFormProps {
-  onCancel: () => void;
-  onSubmit: () => void;
+  setOpen: (open: boolean) => void;
 }
 
-const ProfileEditForm = ({ onCancel, onSubmit }: ProfileEditFormProps) => {
-  // useForm 추가
+const ProfileEditForm = ({ setOpen }: ProfileEditFormProps) => {
+  const { data: userInfo, isPending, isError } = useGetUserInfo();
+  const { mutate: editUserInfo } = useEditUserInfo();
+
+  const [imageFile, setImageFile] = useState<File | string>("");
+  const [profileImageUrl, setProfileImageUrl] = useState<string>(
+    userInfo?.profileImageUrl ?? ""
+  );
+  const fileInputRef = useRef<HTMLInputElement>(null);
+
   const methods = useForm({
     mode: "onChange",
+    defaultValues: {
+      nickname: userInfo?.nickname ?? "",
+      gender: userInfo?.gender ?? "FEMALE",
+      categories: userInfo?.categories ?? [],
+    },
+    resolver: zodResolver(editInfoSchema),
   });
 
-  // handleSubmit 추가
-  const handleSubmit = methods.handleSubmit(onSubmit);
+  const handleSubmit = methods.handleSubmit((data) => {
+    const payload = {
+      profileImage: imageFile,
+      ...data,
+    };
+
+    editUserInfo(payload, {
+      onSuccess: () => {
+        //TODO: 수정 성공 시 토스트 추가
+        setOpen(false);
+      },
+    });
+  });
+
+  if (isPending) return <div>Loading...</div>;
+  if (isError) return <div>Error</div>;
+
+  const handleProfileImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files[0]) {
+      setImageFile(e.target.files[0]);
+      setProfileImageUrl(URL.createObjectURL(e.target.files[0]));
+    }
+  };
+
+  const onCancel = () => {
+    setOpen(false);
+  };
 
   return (
     <FormProvider {...methods}>
       <form
         onSubmit={handleSubmit}
-        className="tb:gap-8 mo:gap-5 flex flex-1 flex-col"
+        className="tb:gap-8 mo:gap-5 flex flex-1 flex-col overflow-y-auto"
       >
         <div className="tb:gap-6 mo:gap-5 flex flex-col">
           <div className="flex justify-center">
             <div className="relative">
-              <SvgProfileF1 className="size-[114px]" />
-              <FileInput
-                name="image"
-                previewClassName="size-[144px] rounded-lg"
-                ButtonComponent={EditButton}
+              <Profile
+                profileImageUrl={profileImageUrl}
+                gender={userInfo.gender}
+                size="lg"
+              />
+              <input
+                type="file"
+                id="profile-image-input"
+                className="hidden"
+                onChange={handleProfileImageChange}
+                ref={fileInputRef}
+              />
+              <EditButton
+                onClick={() => {
+                  fileInputRef.current?.click();
+                }}
               />
             </div>
           </div>
-          <Input name="name" label="닉네임" />
-          <GenderInput className="text-gray-neutral-900 flex justify-center border px-6 py-[14px] font-['Pretendard'] text-base leading-none font-semibold" />
-          <CategoryInput label="선호 카테고리 (2개)" />
+          <Input name="nickname" label="닉네임" />
+          <GenderInput />
+          <CategoryInput label="선호 카테고리 (2개)" type="checkbox" />
         </div>
         <ModalNav
           onCancel={onCancel}
@@ -55,15 +107,3 @@ const ProfileEditForm = ({ onCancel, onSubmit }: ProfileEditFormProps) => {
 };
 
 export default ProfileEditForm;
-
-const EditButton = ({ onClick }: { onClick: () => void }) => {
-  return (
-    <button
-      type="button"
-      onClick={onClick}
-      className="absolute -right-[2px] -bottom-[5px] flex cursor-pointer items-center justify-center rounded-full border border-slate-200 bg-white p-2"
-    >
-      <Edit className="size-6 flex-shrink-0 text-slate-600" />
-    </button>
-  );
-};
