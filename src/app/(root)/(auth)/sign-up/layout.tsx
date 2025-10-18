f765f502968ff8777ee28eca3933fab0b5e18d33
ziyoungsRoom <ilikeeeyo.o@gmail.com>
2025-10-18 15:24:18 +0900

    design: 회원가입 layout 배경 색상 추가

--- src/app/(root)/(auth)/sign-up/layout.tsx
+++ src/app/(root)/(auth)/sign-up/layout.tsx
@@ -6,9 +6,9 @@ const Layout = ({
   children: React.ReactNode;
 }>) => {
   return (
-    <div className="h-screen">
+    <div className="h-screen bg-neutral-50">
       <Header className="absolute top-0 left-0 w-full" />
-      <main className="mx-auto h-full max-w-[1065px] pt-20">{children}</main>
+      <main className="mx-auto mt-20 h-full max-w-[1065px]">{children}</main>
     </div>
   );
 };
