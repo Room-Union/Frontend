b3d5c8f07c5578b321560914c0fe0601074567fe
ziyoungsRoom <ilikeeeyo.o@gmail.com>
2025-10-19 23:23:16 +0900

    design: 이메일 인증코드 step 스타일 추가 및 입력한 email 노출

--- src/components/section/auth/form-container/form-header.tsx
+++ src/components/section/auth/form-container/form-header.tsx
@@ -2,15 +2,17 @@ import { Logo } from "@/assets/icons-colored";
 
 interface FormHeaderProps {
   title?: string;
+  email?: string;
 }
 
-const FormHeader = ({ title }: FormHeaderProps) => {
+const FormHeader = ({ title, email = "" }: FormHeaderProps) => {
   return (
     <div className="flex w-full justify-center">
       {title ? (
-        <h3 className="tb:typo-body-2xl-semibold typo-body-lg-semibold">
-          {title}
-        </h3>
+        <div className="tb:typo-body-2xl-semibold typo-body-lg-semibold text-center">
+          {email && <div>{email}</div>}
+          <h3>{title}</h3>
+        </div>
       ) : (
         <Logo width={"88px"} height={"88px"} className="mb-[10px]" />
       )}
