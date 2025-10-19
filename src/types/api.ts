0b4bd830c93b41aab859716066c6e4e5949e3b89
SongJunHyuk <83599564+J-Moment@users.noreply.github.com>
2025-10-20 05:54:59 +0900

    feat: 모임 리스트 파라미터 type 추가

--- src/types/api.ts
+++ src/types/api.ts
@@ -1,5 +1,15 @@
+import type { CategoryType } from "./constants";
+import type { SortType } from "./gathering-list";
+
 interface Token {
   accessToken: string;
 }
+// 모임 리스트 조회 파라미터
+interface GetGatheringListParams {
+  category?: CategoryType;
+  sort?: SortType;
+  page?: number;
+  size?: number;
+}
 
-export type { Token };
+export type { Token, GetGatheringListParams };
