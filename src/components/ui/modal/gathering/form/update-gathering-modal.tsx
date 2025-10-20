36a041a7fd5c1b852a537023a162d48985f67976
jyooni <berry3891@naver.com>
2025-10-21 00:07:07 +0900

    feat: update-gathering-modal 추가

---
+++ src/components/ui/modal/gathering/form/update-gathering-modal.tsx
@@ -0,0 +1,84 @@
+"use client";
+
+import useUpdateGathering from "@/apis/gathering/mutation/use-update-gathering";
+import { Button, ModalWrapper } from "@/components/ui";
+import GatheringForm from "@/components/ui/modal/gathering/form/gathering-form";
+import {
+  GatheringFormData,
+  GatheringFormInput,
+  GetGatheringDetailResponse,
+} from "@/types/gathering";
+import { useState } from "react";
+
+interface UpdateGatheringModalProps {
+  meetingId: number;
+  data: GetGatheringDetailResponse;
+}
+
+const UpdateGatheringModal = ({
+  meetingId,
+  data,
+}: UpdateGatheringModalProps) => {
+  const [open, setOpen] = useState(false);
+  const { mutate: updateGathering, isPending } = useUpdateGathering();
+
+  // DB에서 받은 값들을 폼에 맞게 변환
+  const defaultValues: GatheringFormInput = {
+    name: data.name,
+    description: data.description,
+    category: [data.category],
+    maxMemberCount: data.maxMemberCount,
+    platformURL: data.platformURL,
+    meetingImage: data.meetingImage,
+  };
+
+  const handleCancel = () => {
+    setOpen(false);
+  };
+
+  const handleSubmit = (formInput: GatheringFormInput) => {
+    console.log("formInput:", formInput);
+
+    // 폼 입력 타입을 API 전송용 타입으로 변환 (배열 → 문자열)
+    const formData: GatheringFormData = {
+      ...formInput,
+      category: formInput.category[0],
+    };
+
+    updateGathering(
+      { meetingId, data: formData },
+      {
+        onSuccess: () => setOpen(false),
+        onError: (error) => console.error(error),
+      }
+    );
+  };
+
+  return (
+    <ModalWrapper
+      open={open}
+      setOpen={setOpen}
+      title="모임 수정하기"
+      description="모임 정보를 수정해보세요"
+      trigger={
+        <Button
+          type="button"
+          variant="primary"
+          size="md"
+          className="tb:h-[60px] tb:rounded-2xl tb:px-[30px] tb:py-4 tb:text-xl mt-[10px] max-w-none"
+          disabled={isPending}
+        >
+          모임 수정하기
+        </Button>
+      }
+    >
+      <GatheringForm
+        onCancel={handleCancel}
+        onSubmit={handleSubmit}
+        defaultValues={defaultValues}
+      />
+    </ModalWrapper>
+  );
+};
+
+export default UpdateGatheringModal;
