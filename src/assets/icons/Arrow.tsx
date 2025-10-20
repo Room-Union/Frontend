ac5f746356935c99d65762872a62b5c2aff78c2b
JIYOON <112847202+jyooni99@users.noreply.github.com>
2025-10-20 15:06:04 +0900

    chore: 아이콘 중복 파일 제거 (#83)
    * chore: 아이콘 중복 파일 제거
    
    * remove: plus 아이콘 제거
    
    * remove: plus 아이콘 추가

--- src/assets/icons/Arrow.tsx
+++
@@ -1,4 +0,0 @@
-import * as React from "react";
-import type { SVGProps } from "react";
-const SvgArrow = (props: SVGProps<SVGSVGElement>) => <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25 24" className="inline-block align-middle" role="img" aria-hidden="true" fill="currentColor" stroke="currentColor" {...props}><path d="M8.17578 10.5356L11.7113 14.0712L15.2468 10.5356" strokeLinecap="round" strokeLinejoin="round" /></svg>;
-export default SvgArrow;
