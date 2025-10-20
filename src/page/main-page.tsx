8f97bd8948a622ab29d74b48138084ba83fa3c04
SongJunHyuk <83599564+J-Moment@users.noreply.github.com>
2025-10-20 10:08:04 +0900

    fix: plus 아이콘 변경

--- src/page/main-page.tsx
+++ src/page/main-page.tsx
@@ -1,5 +1,10 @@
+import { Plus } from "@/assets/icons";
 const MainPage = () => {
-  return <div>MainPage</div>;
+  return (
+    <div>
+      <Plus className="fill-gray-neutral-400 stroke-gray-neutral-400 h-10 w-10" />
+    </div>
+  );
 };
 
 export default MainPage;
