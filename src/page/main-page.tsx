805c55d432839699e2451d374c80c723d3de7cf1
SongJunHyuk <83599564+J-Moment@users.noreply.github.com>
2025-10-20 10:26:16 +0900

    refactor: 메인페이지 plus 아이콘 제거

--- src/page/main-page.tsx
+++ src/page/main-page.tsx
@@ -1,10 +1,5 @@
-import { Plus } from "@/assets/icons";
 const MainPage = () => {
-  return (
-    <div>
-      <Plus className="fill-gray-neutral-400 stroke-gray-neutral-400 h-10 w-10" />
-    </div>
-  );
+  return <div>MainPage</div>;
 };
 
 export default MainPage;
