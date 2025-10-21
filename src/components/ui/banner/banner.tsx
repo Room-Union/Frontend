83d8618f5616c4bdbff5157b15855cb693cf10a7
SongJunHyuk <83599564+J-Moment@users.noreply.github.com>
2025-10-22 08:19:17 +0900

    design: 메인페이지 반응형 구현

--- src/components/ui/banner/banner.tsx
+++ src/components/ui/banner/banner.tsx
@@ -5,7 +5,7 @@ const Banner = ({ className }: { className?: string }) => {
   return (
     <div
       className={cn(
-        "mo:h-[116.357px] pc:h-[244px] tb:h-[141.52px] pc:pl-15 pc:pr-[29.39px] tb:pl-[34.8px] tb:pr-[40.24px] mo:pl-[19px] mo:pr-[21.86px] flex w-full min-w-[375px] items-center justify-between overflow-hidden rounded-[28px] bg-blue-400",
+        "mo:h-[116.357px] pc:h-[244px] tb:h-[141.52px] pc:pl-15 pc:pr-[29.39px] tb:pl-[34.8px] tb:pr-[40.24px] mo:pl-[19px] mo:pr-[21.86px] align-self flex w-full items-center justify-between overflow-hidden rounded-[28px] bg-blue-400",
         className
       )}
     >
@@ -14,10 +14,11 @@ const Banner = ({ className }: { className?: string }) => {
           함께하는 온라인 모임
         </h2>
         <h3 className="pc:typo-body-xl-semibold tb:typo-body-md-medium mo:typo-body-xs-medium text-blue-100">
-          관심사가 같은 사람들과 특별한 시간을 보내보세요
+          관심사가 같은 사람들과
+          <br className="tb:hidden mo:block" /> 특별한 시간을 보내보세요
         </h3>
       </header>
-      <BannerIcon className="pc:h-full tb:h-[144.593px] mo:h-[78.5px] w-auto shrink-0 pt-[7.959px]" />
+      <BannerIcon className="pc:h-full tb:h-[144.593px] mo:h-[78.5px] pc:pt-[7.959px] tb:pt-[4.62] mo:pt:[14.51] w-auto shrink-0" />
     </div>
   );
 };
