8c9ead9a9cacc567ad702259ae09de603464401a
ziyoungsRoom <ilikeeeyo.o@gmail.com>
2025-10-21 19:11:34 +0900

    feat: 인증코드 검증 요청 함수 구현

--- src/components/section/auth/sign-up/email-verification-step.tsx
+++ src/components/section/auth/sign-up/email-verification-step.tsx
@@ -1,21 +1,44 @@
+import { useSendVerificationCode } from "@/apis/auth/mutation/use-send-email";
 import { Input } from "@/components/ui";
 import { inputVariants } from "@/components/ui/input/input";
 import { useFormButtonDisabled } from "@/hooks";
+import { useToastStore } from "@/store/toast-store";
+import { SignUpSchemaType } from "@/validation/sign-up-validation";
+import { UseFormGetValues, UseFormSetError } from "react-hook-form";
 import FormContainer from "../form-container/form-container";
 import FormFooter from "../form-container/form-footer";
 import FormHeader from "../form-container/form-header";
 
 interface EmailVerificationStepProps {
-  email?: string;
   onPrev: () => void;
   onNext: () => void;
+  setError: UseFormSetError<SignUpSchemaType>;
+  getValues: UseFormGetValues<SignUpSchemaType>;
 }
 const EmailVerificationStep = ({
-  email,
+  setError,
+  getValues,
   onPrev,
   onNext,
 }: EmailVerificationStepProps) => {
-  const { isDisabled } = useFormButtonDisabled(["emailVerification"]);
+  const { isDisabled } = useFormButtonDisabled(["verificationCode"]);
+  const [email, verificationCode] = getValues(["email", "verificationCode"]);
+  const { mutate: sendVerificationCode } = useSendVerificationCode();
+  const { toast } = useToastStore();
+
+  const handleNext = async () => {
+    const sendVerificationCodePayload = { email, code: verificationCode };
+    sendVerificationCode(sendVerificationCodePayload, {
+      onSuccess: () => {
+        onNext();
+        toast({
+          message: "이메일 인증이 완료되었습니다.",
+          type: "normal",
+        });
+      },
+    });
+  };
+
   return (
     <FormContainer>
       <FormHeader
@@ -33,7 +56,7 @@ const EmailVerificationStep = ({
       <FormFooter
         text="다음"
         onPrev={onPrev}
-        onNext={onNext}
+        onNext={handleNext}
         isDisabled={isDisabled}
       />
     </FormContainer>
