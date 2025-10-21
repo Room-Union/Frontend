2e8aeca34cb4e9f089f998b647272a3f7efe1870
jyooni <berry3891@naver.com>
2025-10-21 21:58:56 +0900

    refactor: number input 서브 컴포넌트 분리

--- src/components/ui/modal/gathering/form/capacity-url-step.tsx
+++ src/components/ui/modal/gathering/form/capacity-url-step.tsx
@@ -1,23 +1,19 @@
-import { Input } from "@/components/ui";
 import DynamicInput from "@/components/ui/input/dynamic-input";
 import { inputVariants } from "@/components/ui/input/input";
 import Label from "@/components/ui/input/label";
+import { cn } from "@/utils/cn";
+import { useFormContext } from "react-hook-form";
 
 const CapacityUrlStep = () => {
   return (
     <div className="flex flex-col gap-5">
       {/* Max Member Count */}
-      <div className="tb:gap-2 flex flex-col gap-[6px]">
-        <Label text="최대 인원" required htmlFor="maxMemberCount" />
-        <div className="flex w-fit items-center gap-[7px]">
-          <Input
-            name="maxMemberCount"
-            placeholder="최소 2명"
-            className={inputVariants.input.fixed}
-          />
-          <span className="text-base text-zinc-800">명</span>
-        </div>
-      </div>
+      <NumberInput
+        name="maxMemberCount"
+        label="최대 인원"
+        placeholder="최소 2명"
+        unit="명"
+      />
 
       {/* Platform Urls */}
       <div className="tb:gap-2 flex flex-col gap-[6px]">
@@ -27,6 +23,52 @@ const CapacityUrlStep = () => {
           placeholder="https://discord.gg/abce"
         />
       </div>
+    </div>
+  );
+};
+
+//
+interface NumberInputProps {
+  name: string;
+  label: string;
+  placeholder?: string;
+  unit?: string;
+  required?: boolean;
+}
+
+const NumberInput = ({
+  name,
+  label,
+  placeholder,
+  unit = "",
+  required = true,
+}: NumberInputProps) => {
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext();
+
+  const isErrorState = Boolean(errors[name]);
+  const inputBaseStyle = `typo-ui-sm-medium outline-none bg-gray-neutral-50 px-[16px] placeholder:text-gray-neutral-400 focus:ring focus:ring-blue-500 ${isErrorState && "ring ring-red-500"}`;
+
+  return (
+    <div className="tb:gap-2 flex flex-col gap-[6px]">
+      <Label text={label} required={required} htmlFor={name} />
+      <div className="flex items-center gap-[7px]">
+        <input
+          type="text"
+          inputMode="numeric"
+          placeholder={placeholder}
+          className={cn(inputBaseStyle, inputVariants.input.fixed)}
+          {...register(name)}
+        />
+        {unit && <span className="text-base text-zinc-800">{unit}</span>}
+      </div>
+      {errors[name] && (
+        <p className="typo-ui-xs-medium tb:typo-ui-sm-medium px-[4px] text-red-500">
+          {errors[name]?.message as string}
+        </p>
+      )}
     </div>
   );
 };
