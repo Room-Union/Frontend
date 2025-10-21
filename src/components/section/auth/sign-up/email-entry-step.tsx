d6f46fb591fb5c0c013d6d3fa2dbbc673242e4af
ziyoungsRoom <ilikeeeyo.o@gmail.com>
2025-10-21 19:09:38 +0900

    feat: 이메일 인증코드 발송 요청 함수 구현

--- src/components/section/auth/sign-up/email-entry-step.tsx
+++ src/components/section/auth/sign-up/email-entry-step.tsx
@@ -1,17 +1,46 @@
-import { Input } from "@/components/ui";
-
+import { useSendEmail } from "@/apis/auth/mutation/use-send-email";
 import FormContainer from "@/components/section/auth/form-container/form-container";
 import FormFooter from "@/components/section/auth/form-container/form-footer";
 import FormHeader from "@/components/section/auth/form-container/form-header";
+import { Input } from "@/components/ui";
 import { inputVariants } from "@/components/ui/input/input";
 import { useFormButtonDisabled } from "@/hooks";
+import { useToastStore } from "@/store/toast-store";
+import {
+  SendEmailSchemaType,
+  SignUpSchemaType,
+} from "@/validation/sign-up-validation";
+import { UseFormGetValues, UseFormSetError } from "react-hook-form";
 
 interface EmailEntryStepProps {
   onNext: () => void;
+  setError: UseFormSetError<SignUpSchemaType>;
+  getValues: UseFormGetValues<SignUpSchemaType>;
 }
 
-const EmailEntryStep = ({ onNext }: EmailEntryStepProps) => {
+const EmailEntryStep = ({
+  onNext,
+  setError,
+  getValues,
+}: EmailEntryStepProps) => {
   const { isDisabled } = useFormButtonDisabled(["email"]);
+  const email = getValues("email");
+  const { mutate: sendEmail } = useSendEmail();
+  const { toast } = useToastStore();
+
+  // 이메일 인증 코드 발송 요청 api 전송 함수
+  const handleNext = async () => {
+    const sendEmailPayload: SendEmailSchemaType = { email: email };
+    sendEmail(sendEmailPayload, {
+      onSuccess: () => {
+        onNext();
+        toast({
+          message: "이메일이 발송되었습니다.",
+          type: "normal",
+        });
+      },
+    });
+  };
 
   return (
     <FormContainer className="tb:gap-[24px] gap-[24px]">
@@ -26,7 +55,7 @@ const EmailEntryStep = ({ onNext }: EmailEntryStepProps) => {
         />
         <FormFooter
           text="다음"
-          onNext={onNext}
+          onNext={handleNext}
           href={"/sign-in"}
           isDisabled={isDisabled}
           isFirstStep
