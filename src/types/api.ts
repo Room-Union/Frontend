684173c541604f2134982b7cb2d59b9141385b55
SongJunHyuk <83599564+J-Moment@users.noreply.github.com>
2025-10-21 10:38:25 +0900

    refactor: GetGatheringListParams 제거

--- src/types/api.ts
+++ src/types/api.ts
@@ -1,15 +1,6 @@
-import type { CategoryType } from "./constants";
-import type { SortType } from "./gathering-list";
-
 interface Token {
   accessToken: string;
 }
-// 모임 리스트 조회 파라미터
-interface GetGatheringListParams {
-  category?: CategoryType;
-  sort?: SortType;
-  page?: number;
-  size?: number;
-}
+// 모임 리스트 조회 파라미
 
-export type { Token, GetGatheringListParams };
+export type { Token };
