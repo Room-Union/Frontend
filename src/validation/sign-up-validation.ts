9ceeded0be04cca7d79084f6a2b872d176fe43b2
ziyoungsRoom <ilikeeeyo.o@gmail.com>
2025-10-21 18:52:59 +0900

    feat: email 인증코드 검증 schema 생성 및 타입 추가

--- src/validation/sign-up-validation.ts
+++ src/validation/sign-up-validation.ts
@@ -28,6 +28,12 @@ export const profileEntrySchema = z.object({
   gender: genderSchema,
 });
 
+// 이메일 인증 코드 검증 스키마
+export const EmailVerificationSchema = z.object({
+  ...emailEntrySchema.shape,
+  ...verificationCodeEntrySchema.shape,
+});
+
 // 회원가입 전체 스키마 : 각 스텝별 스키마 배열
 export const signUpSchema = z
   .object({
@@ -40,6 +46,14 @@ export const signUpSchema = z
     path: ["confirmPassword"],
     message: "비밀번호가 일치하지 않습니다.",
   });
+
+// 이메일 전송 스키마 타입
+export type SendEmailSchemaType = z.infer<typeof emailEntrySchema>;
+
+// 이메일 인증코드 검증 스키마 타입
+export type SendVerificationCodeSchemaType = z.infer<
+  typeof EmailVerificationSchema
+>;
 
 // 회원가입 전체 스키마 타입 : 유니온 타입
 export type SignUpSchemaType = z.infer<typeof signUpSchema>;
