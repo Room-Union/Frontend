e3fe7f6968b489e08543576f46cd9d8862f8354c
jyooni <berry3891@naver.com>
2025-10-23 23:31:17 +0900

    chore: index.ts에 컴포넌트 추가

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -30,6 +30,8 @@ import ModalNav from "./modal/modal-nav";
 import ModalWrapper from "./modal/modal-wrapper";
 import PasswordEditModal from "./modal/user/password-edit/password-edit-modal";
 import ProfileEditModal from "./modal/user/profile-edit/profile-edit-modal";
+import DatePicker from "./picker/date-picker";
+import DateTimePicker from "./picker/date-time-picker";
 import CardProfile from "./profile/card-profile";
 import Profile from "./profile/profile";
 import Progress from "./progress/progress";
@@ -48,6 +50,8 @@ export {
   CategoryButton,
   CategoryInput,
   CreateGatheringModal,
+  DatePicker,
+  DateTimePicker,
   Dropdown,
   DynamicInput,
   FileInput,
--- src/components/ui/picker/date-picker.tsx
+++ src/components/ui/picker/date-picker.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { ArrowLeft, ArrowRight } from "@/assets/icons";
 import { cn } from "@/utils/cn";
 import {
