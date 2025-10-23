4c3503ad67850dc420d405ff4cc525f1b548df1c
ziyoungsRoom <ilikeeeyo.o@gmail.com>
2025-10-23 19:04:47 +0900

    feat: signInSchema error message 하나로 출력되도록 수정

--- src/validation/sign-in-validation.ts
+++ src/validation/sign-in-validation.ts
@@ -1,9 +1,31 @@
 import z from "zod";
-import { emailSchema, passwordSchema } from "./validation";
+import { EMAIL_REGEX, PASSWORD_REGEX } from "./validation";
 
-export const signInSchema = z.object({
-  email: emailSchema,
-  password: passwordSchema,
-});
+export const signInSchema = z
+  .object({
+    email: z.string().trim(),
+    password: z.string().trim(),
+  })
+  .superRefine((data, ctx) => {
+    if (
+      !(
+        EMAIL_REGEX.test(data.email) &&
+        8 <= data.password.length &&
+        data.password.length <= 13 &&
+        PASSWORD_REGEX.test(data.password)
+      )
+    ) {
+      ctx.addIssue({
+        code: "custom",
+        path: ["email"],
+      });
+
+      ctx.addIssue({
+        code: "custom",
+        path: ["password"],
+        message: "아이디 혹은 비밀번호가 일치하지 않습니다.",
+      });
+    }
+  });
 
 export type SignInSchemaType = z.infer<typeof signInSchema>;
