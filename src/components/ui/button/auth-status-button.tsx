be9e888174f12d02228f054a4b19a5ecfe46a207
정민석 <minskyson7@gmail.com>
2025-10-24 00:15:30 +0900

    feat: 헤더 프로필 클릭 시 드롭다운 구현

--- src/components/ui/button/auth-status-button.tsx
+++ src/components/ui/button/auth-status-button.tsx
@@ -1,8 +1,9 @@
 "use client";
 import useGetUserInfo from "@/apis/user/query/use-get-user-info";
-import { Button, Profile } from "@/components/ui";
+import { Button, Dropdown, Profile } from "@/components/ui";
+import useLogout from "@/hooks/use-logout";
 import { checkIsSignedIn } from "@/utils/auth";
-import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 interface AuthStatusButtonProps {
   className?: string;
@@ -11,15 +12,46 @@ interface AuthStatusButtonProps {
 const AuthStatusButton = ({ className }: AuthStatusButtonProps) => {
   const isSignedIn = checkIsSignedIn();
   const { data } = useGetUserInfo();
+  const router = useRouter();
+  const { handleLogout } = useLogout();
+  const handleDropDown = (value: string) => {
+    switch (value) {
+      case "mypage":
+        router.push("/my-page");
+        break;
+      case "signout":
+        handleLogout();
+        break;
+    }
+  };
   if (isSignedIn && data) {
     return (
-      <Link href="/my-page" className={className}>
-        <Profile
-          gender={data?.gender}
-          profileImageUrl={data?.profileImageUrl}
-          size="sm"
-        />
-      </Link>
+      <Dropdown
+        onValueChange={handleDropDown}
+        trigger={
+          <button className="mo:hidden tb:block outline-none">
+            <Profile
+              gender={data?.gender}
+              profileImageUrl={data?.profileImageUrl}
+              size="sm"
+            />
+          </button>
+        }
+        selectItems={[
+          {
+            value: "mypage",
+            text: "마이페이지",
+          },
+          {
+            value: "signout",
+            text: "로그아웃",
+          },
+        ]}
+        dropDown
+        contentAlign="end"
+        dropDownClassName="typo-ui-md-semibold text-gray-neutral-500 hover:text-gray-neutral-700"
+        size="md"
+      />
     );
   } else {
     return (
@@ -31,3 +63,11 @@ const AuthStatusButton = ({ className }: AuthStatusButtonProps) => {
 };
 
 export default AuthStatusButton;
+
+{
+  /* <Profile
+gender={data?.gender}
+profileImageUrl={data?.profileImageUrl}
+size="sm"
+/> */
+}
