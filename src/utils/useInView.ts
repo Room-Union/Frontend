07302f86eb28a1d001b02413befcf21fcea99e02
SongJunHyuk <83599564+J-Moment@users.noreply.github.com>
2025-10-27 14:11:17 +0900

    refactor: intersection Observer를 활용하도록 수정

--- src/utils/useInView.ts
+++ src/utils/useInView.ts
@@ -18,35 +18,29 @@ const useInView = (): UseInViewReturn => {
     // ref가 아직 연결되지 않았으면 종료
     if (!element) return;
 
-    // rootMargin: 600픽셀 이내에 있으면 감지
-    const rootMargin = 400;
+    const observer = new IntersectionObserver(
+      // callBack
+      (entries) => {
+        entries.forEach((entry) => {
+          // 요소가 뷰포트에 들어왔을때 true, 아니면 false
+          setIsInView(entry.isIntersecting);
+        });
+      },
+      //options
+      {
+        // 요소의 범위를 400px 확장.
+        rootMargin: "400px",
+        // 타겟이 관찰되기 시작하자마자 옵저버
+        threshold: 0,
+      }
+    );
 
-    // 요소가 뷰포트 들어왔는지 확인
-    const checkInView = () => {
-      // 요소의 하단 좌표 = 맨 위로부터 기준으로부터 떨어진 위치 + targetRef 요소의 높이
-      const elementBottom = element.offsetTop + element.offsetHeight;
-      // 현재 화면의 하단좌표 = 스크롤양 + 브라우저의 뷰포트 높이(innerHeight) || (IE의 경우)document.documentElement.clientHeight
-      const windowBottom =
-        window.scrollY +
-        (window.innerHeight || document.documentElement.clientHeight);
+    // 요소 관찰 시작
+    observer.observe(element);
 
-      // 현재 화면 하단 + 마진값이 요소의 위치보다 크거나 같으면 isVisible 업데이트
-      const isVisible = windowBottom + rootMargin >= elementBottom;
-
-      setIsInView(isVisible);
-    };
-
-    checkInView();
-
-    // 스크롤 이벤트 발생 시 checkInView 함수 실행
-    window.addEventListener("scroll", checkInView);
-    // 윈도우 크기 변경 시 checkInView 함수 실행
-    window.addEventListener("resize", checkInView);
-
-    // useEffect의 cleanup 함수: 컴포넌트 언마운트 시 이벤트 리스너 제거
     return () => {
-      window.removeEventListener("scroll", checkInView);
-      window.removeEventListener("resize", checkInView);
+      observer.unobserve(element);
+      observer.disconnect();
     };
   }, []);
 
