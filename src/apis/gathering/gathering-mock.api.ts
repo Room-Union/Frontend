9888f7703ba64f8ef244cb703521d53f6b01c0fc
jyooni <berry3891@naver.com>
2025-10-27 21:21:23 +0900

    fix: api endpoint 변경

--- src/apis/gathering/gathering-mock.api.ts
+++ src/apis/gathering/gathering-mock.api.ts
@@ -1,3 +1,4 @@
+import { api } from "@/apis/api";
 import {
   DeleteGatheringRequest,
   GatheringFormData,
@@ -6,11 +7,10 @@ import {
   JoinGatheringRequest,
   UpdateGatheringRequest,
 } from "@/types/gathering";
-import axios from "axios";
 
 // 모임 생성 테스트
 const createGatheringTest = async (data: GatheringFormData) => {
-  const res = await axios.post("http://localhost:4000/gathering/create", data);
+  const res = await api.post(`/meetings`, data);
   return res.data;
 };
 
@@ -18,9 +18,7 @@ const createGatheringTest = async (data: GatheringFormData) => {
 const getGatheringDetailTest = async (
   meetingId: GetGatheringDetailRequest
 ): Promise<GetGatheringDetailResponse> => {
-  const res = await axios.get(
-    `http://localhost:4000/gathering/detail/${meetingId}`
-  );
+  const res = await api.get(`/meetings/${meetingId}`);
   return res.data;
 };
 
@@ -29,26 +27,19 @@ const updateGatheringTest = async ({
   meetingId,
   data,
 }: UpdateGatheringRequest) => {
-  const res = await axios.put(
-    `http://localhost:4000/gathering/detail/${meetingId}`,
-    { data }
-  );
+  const res = await api.put(`/meetings/${meetingId}`, { data });
   return res.data;
 };
 
 // 모임 삭제 테스트
 const deleteGatheringTest = async (meetingId: DeleteGatheringRequest) => {
-  const res = await axios.delete(
-    `http://localhost:4000/gathering/detail/${meetingId}`
-  );
+  const res = await api.delete(`/meetings/${meetingId}`);
   return res.data;
 };
 
 // 모임 참가 테스트
 const joinGatheringTest = async (meetingId: JoinGatheringRequest) => {
-  const res = await axios.post(
-    `http://localhost:4000/gathering/${meetingId}/join`
-  );
+  const res = await api.post(`/meetings/${meetingId}/join`);
   return res.data;
 };
 
