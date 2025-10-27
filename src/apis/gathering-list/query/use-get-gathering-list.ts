7c4d9a8a51a9ad463fbf36353cb0927cc90e4051
정민석 <48466548+phantom2115@users.noreply.github.com>
2025-10-27 15:30:54 +0900

    api: 나의 모임 리스트 api 연동 (#114)
    * feat: 나의 모임 api 및 type 추가
    
    * feat: 나의 모임 usequery 커스텀 훅 작성
    
    * feat: 마이페이지 나의 모임 api 연동

--- src/apis/gathering-list/query/use-get-gathering-list.ts
+++ src/apis/gathering-list/query/use-get-gathering-list.ts
@@ -1,7 +1,7 @@
 import queryKeys from "@/apis/query-keys";
 import type { GetGatheringListRequest } from "@/types/gathering-list";
 import { useQuery } from "@tanstack/react-query";
-import getGatheringListInfo from "../gathering-list.api";
+import { getGatheringListInfo } from "../gathering-list.api";
 
 // 모임 리스트 조회 query
 const useGetGatheringListInfo = (params: GetGatheringListRequest) => {
