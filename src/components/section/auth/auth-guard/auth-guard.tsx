a30aef1364dc24a92925abd99b8f5a6e6409dfe4
ziyoungsRoom <ilikeeeyo.o@gmail.com>
2025-10-30 09:20:01 +0900

    feat: 토큰 검증 로직 추가

--- src/components/section/auth/auth-guard/auth-guard.tsx
+++ src/components/section/auth/auth-guard/auth-guard.tsx
@@ -1,4 +1,6 @@
 "use client";
+import useGetUserInfo from "@/apis/user/query/use-get-user-info";
+import { Spinner } from "@/components/ui";
 import { getAccessToken } from "@/utils/auth";
 import { usePathname, useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
@@ -7,21 +9,27 @@ function AuthGuard({ children }: { children: React.ReactNode }) {
   const router = useRouter();
   const pathname = usePathname();
   const [checking, setChecking] = useState(true);
+  const { data, isLoading } = useGetUserInfo();
 
   useEffect(() => {
     const token = getAccessToken();
     const isAuthPage = pathname === "/sign-in" || pathname === "/sign-up";
 
+    // isLoading 상태 : 로딩 상태 유지
+    if (isLoading) {
+      return;
+    }
+
     if (!token && !isAuthPage) {
       router.replace("/sign-in");
     } else if (token && isAuthPage) {
-      router.replace("/");
-    } else {
+      if (data) router.replace("/");
+    } else if (checking) {
       setChecking(false);
     }
-  }, [pathname, router]);
+  }, [pathname, router, data, isLoading]);
 
-  if (checking)
+  if (checking || isLoading)
     return (
       <div className="typo-ui-xs-medium tb:typo-ui-md-medium text-gray-neutral-400 flex h-full min-h-screen w-full flex-col items-center justify-center gap-5">
         <Spinner variant={"outline"} size={"page"} />
