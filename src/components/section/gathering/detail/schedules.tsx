92889ea087d1ca8c441c64080960e0dd155711c4
jyooni <berry3891@naver.com>
2025-10-31 19:34:28 +0900

    HOTFIX: 빌드 오류 해결

--- src/components/section/gathering/detail/schedules.tsx
+++ src/components/section/gathering/detail/schedules.tsx
@@ -1,18 +1,25 @@
 import { CalendarX } from "@/assets/icons";
 import MeetUpCard from "@/components/ui/card/meet-up-card";
-import { Schedule } from "@/types/schedules";
+import { GetAppointmentResponse } from "@/types/appointments";
 
 const Schedules = () => {
-  const schedules: Schedule[] = [
+  const schedules: GetAppointmentResponse[] = [
     {
       id: 1,
       title: "모임 일정",
       scheduledAt: "2021-01-01T00:00:00.000Z",
       currentMemberCount: 1,
       maxMemberCount: 10,
-      scheduleImage: "",
+      creatorId: 1,
+      imageUrl: "",
+      isJoined: false,
     },
   ]; // Todo: 모임 일정 데이터 추가하기
+
+  // TODO: 실제 데이터로 대체 필요
+  const isOwner = false;
+  const meetingId = 0;
+  const isGatheringJoined = false;
 
   if (schedules.length === 0) {
     return (
@@ -32,7 +39,13 @@ const Schedules = () => {
   return (
     <>
       {schedules.map((schedule) => (
-        <MeetUpCard key={schedule.id} data={schedule} />
+        <MeetUpCard
+          key={schedule.id}
+          data={schedule}
+          isOwner={isOwner}
+          meetingId={meetingId}
+          isGatheringJoined={isGatheringJoined}
+        />
       ))}
     </>
   );
