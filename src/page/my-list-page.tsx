0ac993bee12400281c8278b6bc14a88858bddee2
정민석 <minskyson7@gmail.com>
2025-11-02 21:23:21 +0900

    feat: 역할 기반 모임 리스트 조회

--- src/page/my-list-page.tsx
+++ src/page/my-list-page.tsx
@@ -1,7 +1,36 @@
-import { AuthGuard } from "@/components/section";
+"use client";
+import useGetGatheringMineList from "@/apis/gathering-list/query/use-get-gathering-mine-list";
+import { AuthGuard, GatheringGrid } from "@/components/section";
+import { RoleType } from "@/types/constants";
+import { useSearchParams } from "next/navigation";
 
 const MyListPage = () => {
-  return <AuthGuard>MyListPage</AuthGuard>;
+  const searchParams = useSearchParams();
+  const role = searchParams.get("role")?.toUpperCase() as RoleType;
+  const { data, isLoading, isError } = useGetGatheringMineList({
+    role,
+    page: 0,
+    size: 10,
+  });
+
+  const title = {
+    HOST: "내가 생성한 모임",
+    MEMBER: "내가 가입한 모임",
+  };
+
+  if (isLoading) return <div>Loading...</div>;
+  if (isError) return <div>Error</div>;
+
+  return (
+    <AuthGuard>
+      <div className="pc:py-17.5 tb:py-11.5 mo:py-6 pc:gap-11.5 tb:gap-8.5 mo:gap-7.5 flex w-full flex-col">
+        <h2 className="pc:typo-ui-2xl-semibold tb:typo-ui-xl-semibold mo:typo-ui-lg-semibold text-gray-neutral-900">
+          {title[role]}
+        </h2>
+        <GatheringGrid gatheringList={data?.content} />
+      </div>
+    </AuthGuard>
+  );
 };
 
 export default MyListPage;
