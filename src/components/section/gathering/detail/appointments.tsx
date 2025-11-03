2e6e0bf365a38d943f404e0d83a20e8be7e43178
jyooni <berry3891@naver.com>
2025-11-03 14:05:33 +0900

    refactor: suspense 및 error boundary 추가

--- src/components/section/gathering/detail/appointments.tsx
+++ src/components/section/gathering/detail/appointments.tsx
@@ -1,6 +1,5 @@
 import useGetAppointments from "@/apis/appointments/query/use-get-appointments";
 import { CalendarX } from "@/assets/icons";
-import { DetailSection } from "@/components/section";
 import { Carousel, MeetUpCard } from "@/components/ui";
 import { GetAppointmentResponse } from "@/types/appointments";
 
@@ -15,44 +14,37 @@ const Appointments = ({ meetingId, isOwner, isJoined }: AppointmentsProps) => {
 
   if (!appointments || appointments.length === 0) {
     return (
-      <DetailSection title="모임 약속">
-        <div className="mb-5 flex h-[54px] flex-col items-center justify-center gap-[10px] pt-1">
-          <CalendarX
-            className="size-[30px] stroke-none text-neutral-300"
-            role="img"
-            aria-hidden="true"
-          />
-          <p className="typo-ui-sm-semibold text-neutral-300">
-            생성된 약속이 없습니다.
-          </p>
-        </div>
-      </DetailSection>
+      <div className="mb-5 flex h-[54px] flex-col items-center justify-center gap-[10px] pt-1">
+        <CalendarX
+          className="size-[30px] stroke-none text-neutral-300"
+          role="img"
+          aria-hidden="true"
+        />
+        <p className="typo-ui-sm-semibold text-neutral-300">
+          생성된 약속이 없습니다.
+        </p>
+      </div>
     );
   }
 
   return (
-    <DetailSection title="모임 약속">
-      <section className="pc:mx-0 tb:-mx-6 mo:-mx-5 pt-1">
-        <Carousel
-          listType="appointmentList"
-          totalItemCount={appointments.length}
-        >
-          {appointments.map((appointment: GetAppointmentResponse) => (
-            <li
-              key={appointment.id}
-              className="pc:first:ml-0 tb:first:ml-6 mo:first:ml-5 pc:last:mr-0 tb:last:mr-6 mo:last:mr-5"
-            >
-              <MeetUpCard
-                data={appointment}
-                isGatheringJoined={isJoined}
-                isOwner={isOwner}
-                meetingId={meetingId}
-              />
-            </li>
-          ))}
-        </Carousel>
-      </section>
-    </DetailSection>
+    <section className="pc:mx-0 tb:-mx-6 mo:-mx-5 pt-1">
+      <Carousel listType="appointmentList" totalItemCount={appointments.length}>
+        {appointments.map((appointment: GetAppointmentResponse) => (
+          <li
+            key={appointment.id}
+            className="pc:first:ml-0 tb:first:ml-6 mo:first:ml-5 pc:last:mr-0 tb:last:mr-6 mo:last:mr-5"
+          >
+            <MeetUpCard
+              data={appointment}
+              isGatheringJoined={isJoined}
+              isOwner={isOwner}
+              meetingId={meetingId}
+            />
+          </li>
+        ))}
+      </Carousel>
+    </section>
   );
 };
 
