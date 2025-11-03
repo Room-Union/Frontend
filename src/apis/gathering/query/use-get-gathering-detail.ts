7bc9d9ce3fa9545a38f1631299661ef7715d191c
jyooni <berry3891@naver.com>
2025-11-03 13:23:22 +0900

    feat: useSuspenseQuery로 변경

--- src/apis/gathering/query/use-get-gathering-detail.ts
+++ src/apis/gathering/query/use-get-gathering-detail.ts
@@ -1,13 +1,13 @@
 import { getGatheringDetail } from "@/apis/gathering/gathering.api";
 import queryKeys from "@/apis/query-keys";
 import { GetGatheringDetailRequest } from "@/types/gathering";
-import { useQuery } from "@tanstack/react-query";
+import { useSuspenseQuery } from "@tanstack/react-query";
 
 const useGetGatheringDetail = (meetingId: GetGatheringDetailRequest) => {
-  return useQuery({
+  return useSuspenseQuery({
     queryKey: queryKeys.gathering.detail(meetingId),
     queryFn: () => getGatheringDetail(meetingId),
-    enabled: !!meetingId,
+    retry: false,
   });
 };
 
