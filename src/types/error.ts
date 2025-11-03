d7ead7f5833f940dba76fa46b3f6ac5b9d2c80e2
ziyoungsRoom <ilikeeeyo.o@gmail.com>
2025-11-03 11:02:58 +0900

    feat: 에러 타입 작성

--- src/types/error.ts
+++ src/types/error.ts
@@ -1,13 +1,25 @@
-interface FieldErrorType {
-  defaultType: "field";
-  field: string;
-  message: string;
-}
+import { ERROR_MESSAGES } from "@/constants/error-message";
+import { FieldPath, FieldValues } from "react-hook-form";
 
-interface ToastErrorType {
+export type ErrorMessageMap = typeof ERROR_MESSAGES;
+export type ErrorCode = keyof ErrorMessageMap;
+export type ErrorMessage = ErrorMessageMap[ErrorCode];
+
+export type ErrorToast = {
   defaultType: "toast";
   message: string;
   subMessage?: string;
-}
+};
 
-export type ErrorType = ToastErrorType | FieldErrorType;
+export type ErrorField<T extends FieldValues> = {
+  defaultType: "field";
+  field: FieldPath<T>;
+  message: string;
+  subMessage?: string;
+};
+
+export interface OverrideFieldError<T extends FieldValues> {
+  code: ErrorCode;
+  message?: string;
+  field: FieldPath<T>;
+}
