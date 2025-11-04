8bc1042b77bd46e15adadc7d39506dae1890d15a
jyooni <berry3891@naver.com>
2025-11-04 10:45:27 +0900

    feat: appointments, members skeleton 구현

--- src/components/section/fallback/gathering-detail-skeleton.tsx
+++ src/components/section/fallback/gathering-detail-skeleton.tsx
@@ -1,11 +1,15 @@
+import DetailSection from "@/components/section/gathering/detail/detail-section";
+import AppointmentsSkeleton from "./appointments-skeleton";
+import MembersSkeleton from "./members-skeleton";
+
 const GatheringDetailSkeleton = () => {
   return (
-    <div className="pc:max-w-[1280px] pc:pt-12 tb:pt-9 mo:pt-[25px] pc:pb-[30.5px] mx-auto animate-pulse bg-white pb-0">
+    <div className="pc:max-w-[1280px] pc:pt-12 tb:pt-9 mo:pt-[25px] pc:pb-[30.5px] mx-auto bg-white pb-0">
       <div className="pc:flex-row flex flex-col justify-between gap-[30px]">
         {/* Main Content Skeleton */}
         <div className="tb:px-0 pc:max-w-[790px] pc:flex-1 pc:min-w-0 w-full">
           {/* Header Skeleton */}
-          <div className="mb-6">
+          <div className="mb-6 animate-pulse">
             {/* Image Skeleton */}
             <div className="tb:rounded-3xl relative aspect-[4/3] w-full overflow-hidden rounded-2xl bg-neutral-200" />
 
@@ -20,31 +24,46 @@ const GatheringDetailSkeleton = () => {
           </div>
 
           {/* Information (태블릿 이하) */}
-          <div className="pc:hidden tb:py-5 mb-6 rounded-2xl border-neutral-200 py-2">
+          <div className="pc:hidden tb:py-5 mb-6 animate-pulse rounded-2xl border-neutral-200 py-2">
             <div className="space-y-3">
               <div className="h-5 w-24 rounded bg-neutral-200" />
               <div className="h-5 w-36 rounded bg-neutral-200" />
+              <div className="h-5 w-28 rounded bg-neutral-200" />
             </div>
           </div>
 
           {/* Description Skeleton */}
-          <div className="tb:py-9 mb-6 rounded-2xl border-neutral-200 py-4">
-            <div className="mb-4 h-6 w-24 rounded bg-neutral-200" />
-            <div className="space-y-2">
+          <DetailSection>
+            <div className="animate-pulse space-y-2 pt-[30px]">
               <div className="h-4 w-full rounded bg-neutral-200" />
               <div className="h-4 w-full rounded bg-neutral-200" />
               <div className="h-4 w-2/3 rounded bg-neutral-200" />
             </div>
-          </div>
+          </DetailSection>
+
+          {/* Members Skeleton */}
+          <DetailSection>
+            <div className="pt-[30px]">
+              <MembersSkeleton />
+            </div>
+          </DetailSection>
+
+          {/* Appointments Skeleton */}
+          <DetailSection>
+            <div className="pt-[30px]">
+              <AppointmentsSkeleton />
+            </div>
+          </DetailSection>
         </div>
 
         {/* Sidebar Skeleton */}
         <div className="pc:top-[30px] pc:w-[380px] pc:mx-0 pc:h-fit pc:rounded-[20px] pc:border pc:border-neutral-200 pc:p-6 tb:-mx-6 mo:-mx-5 tb:py-6 sticky bottom-0 z-10 border-t border-neutral-200 bg-white py-3">
           <div className="tb:px-6 mo:px-5 pc:px-0">
             {/* Information (PC) */}
-            <div className="pc:block pc:pt-2 hidden space-y-7">
+            <div className="pc:block pc:pt-2 hidden animate-pulse space-y-7">
               <div className="h-5 w-24 rounded bg-neutral-200" />
               <div className="h-5 w-36 rounded bg-neutral-200" />
+              <div className="h-5 w-28 rounded bg-neutral-200" />
               <div className="tb:h-[60px] tb:rounded-2xl h-12 w-full rounded-xl bg-neutral-200" />
             </div>
           </div>
