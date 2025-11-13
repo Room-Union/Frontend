00e1c7156ac6a7aa31eeaec4c5213e560a063749
jyooni <berry3891@naver.com>
2025-11-13 17:55:31 +0900

    fix: msw 로직 변경

--- src/mocks/index.ts
+++ src/mocks/index.ts
@@ -2,7 +2,7 @@ const USE_MSW = false;
 
 export async function initMocks() {
   if (process.env.NODE_ENV !== "development") return;
-  if (USE_MSW) return;
+  if (!USE_MSW) return;
 
   if (typeof window === "undefined") {
     const { server } = await import("./server");
