5840e52bb2aef9fd4f30b70df1982a40be61c3f8
ziyoungsRoom <ilikeeeyo.o@gmail.com>
2025-11-15 02:50:14 +0900

    test: 로그인 유효성 검사 테스트 코드 작성

--- src/components/section/auth/sign-in/sign-in-form.test.tsx
+++ src/components/section/auth/sign-in/sign-in-form.test.tsx
@@ -30,4 +30,34 @@ describe("SignInForm 컴포넌트 테스트", () => {
       expect(loginButton).toBeEnabled();
     });
   });
+
+  describe("유효성 검사 테스트", () => {
+    test("유효하지 않은 이메일 입력 시 오류 메시지 노출되는지 확인", async () => {
+      fireEvent.change(emailInput, { target: { value: "invalid-email" } });
+      fireEvent.change(passwordInput, {
+        target: { value: "validPassword123!" },
+      });
+
+      fireEvent.click(loginButton);
+
+      const ErrorMessage = await screen.findByText(
+        "아이디 혹은 비밀번호가 일치하지 않습니다."
+      );
+      expect(ErrorMessage).toBeInTheDocument();
+    });
+
+    test("유효하지 않은 비밀번호 입력 시 오류 메시지 노출되는지 확인", async () => {
+      fireEvent.change(emailInput, { target: { value: "valid@email.com" } });
+      fireEvent.change(passwordInput, {
+        target: { value: "invalid-password" },
+      });
+
+      fireEvent.click(loginButton);
+
+      const ErrorMessage = await screen.findByText(
+        "아이디 혹은 비밀번호가 일치하지 않습니다."
+      );
+      expect(ErrorMessage).toBeInTheDocument();
+    });
+  });
 });
