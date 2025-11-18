6ab2c600c2bc263666e49f1d8c5f23e8895bce1d
ziyoungsRoom <ilikeeeyo.o@gmail.com>
2025-11-18 18:37:11 +0900

    test: EmailEntryStep 버튼 비활성화 테스트 코드 작성

--- src/components/section/auth/sign-up/email-entry-step.test.tsx
+++ src/components/section/auth/sign-up/email-entry-step.test.tsx
@@ -1,7 +1,7 @@
 import { signUpFormOptions } from "@/form-options/sign-up-form-option";
 import ReactHookFormProvider from "@/providers/reacthookform-provider";
 import { SignUpSchemaType } from "@/types/schema";
-import { screen } from "@testing-library/react";
+import { fireEvent, screen, waitFor } from "@testing-library/react";
 import { renderWithQueryClient } from "../../../../../jest.setup";
 import EmailEntryStep from "./email-entry-step";
 
@@ -17,7 +17,27 @@ describe("EmailEntryStep 컴포넌트 테스트", () => {
       </ReactHookFormProvider>
     );
 
-    emailInput = screen.getByLabelText("이메일");
+    emailInput = screen.getByLabelText(/이메일/i);
     nextButton = screen.getByRole("button", { name: "다음" });
+  });
+
+  describe("버튼 비활성화 테스트", () => {
+    test("이메일 미입력 시 다음 버튼 비활성화되는지 확인", () => {
+      expect(emailInput).toHaveValue("");
+
+      expect(nextButton).toBeDisabled();
+    });
+
+    test("이메일 형식 미충족 시 다음 버튼 비활성화되는지 확인", async () => {
+      fireEvent.change(emailInput, { target: { value: "email" } });
+
+      await waitFor(() => expect(nextButton).toBeDisabled());
+    });
+
+    test("이메일 형식 충족 시 다음 버튼 활성화되는지 확인", () => {
+      fireEvent.change(emailInput, { target: { value: "email@test.com" } });
+
+      expect(nextButton).toBeEnabled();
+    });
   });
 });
