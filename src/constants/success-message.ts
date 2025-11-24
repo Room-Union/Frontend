2d9c9620935452a7e0b12daecea6a03ed85ca77e
jyooni <berry3891@naver.com>
2025-11-24 17:38:17 +0900

    chore: 메세지 자연스럽게 수정

--- src/constants/success-message.ts
+++ src/constants/success-message.ts
@@ -5,7 +5,7 @@ export const GATHERING_SUCCESS_MESSAGES = {
   UPDATE: { type: "normal", message: "모임을 수정했어요!" },
   DELETE: { type: "normal", message: "모임을 삭제했어요!" },
   JOIN: { type: "normal", message: "모임에 참여했어요!" },
-  LEAVE: { type: "normal", message: "모임을 나갔어요!" },
+  LEAVE: { type: "normal", message: "모임에서 탈퇴했어요!" },
 } as const satisfies Record<string, ToastParams>;
 
 export const APPOINTMENT_SUCCESS_MESSAGES = {
@@ -13,5 +13,5 @@ export const APPOINTMENT_SUCCESS_MESSAGES = {
   UPDATE: { type: "normal", message: "약속을 수정했어요!" },
   DELETE: { type: "normal", message: "약속을 삭제했어요!" },
   JOIN: { type: "normal", message: "약속에 참여했어요!" },
-  LEAVE: { type: "normal", message: "약속을 나갔어요!" },
+  LEAVE: { type: "normal", message: "약속 참여를 취소했어요!" },
 } as const satisfies Record<string, ToastParams>;
