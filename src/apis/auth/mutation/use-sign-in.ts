8af4a2c7d305e900bbd9473c6b7ea4a69c22c99a
ziyoungsRoom <ilikeeeyo.o@gmail.com>
2026-02-06 21:05:46 +0900

    feat: 로그인 / 로그아웃 / refreshToken 만료 시 state 업데이트

--- src/apis/auth/mutation/use-sign-in.ts
+++ src/apis/auth/mutation/use-sign-in.ts
@@ -1,3 +1,4 @@
+import { useAuthStore } from "@/store/auth-store";
 import { SignInRequest } from "@/types/auth";
 import { useMutation } from "@tanstack/react-query";
 import { signInUser } from "../auth.api";
@@ -5,6 +6,7 @@ import { signInUser } from "../auth.api";
 const useSignIn = () => {
   return useMutation({
     mutationFn: (data: SignInRequest) => signInUser(data),
+    onSuccess: () => useAuthStore.getState().setSignedIn(),
   });
 };
 
