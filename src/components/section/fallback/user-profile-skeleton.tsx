4bec0e8ce8548a75296420a6558ae443e14319e4
정민석 <minskyson7@gmail.com>
2026-02-24 09:41:39 +0900

    ui: UserProfile 및 UserProfileSkeleton 컴포넌트의 높이 조정

--- src/components/section/fallback/user-profile-skeleton.tsx
+++ src/components/section/fallback/user-profile-skeleton.tsx
@@ -1,7 +1,7 @@
 const UserProfileSkeleton = () => {
   return (
     <section className="tb:flex w-full items-center justify-center">
-      <div className="mo:self-stretch tb:self-auto pc:px-13 pc:py-7 tb:px-[30px] tb:py-6 mo:px-5 mo:py-6 pc:gap-6 tb:gap-5 mo:gap-[19.47px] bg-blue-25 pc:rounded-[50px] tb:rounded-[36px] mo:rounded-[30px] tb:w-[385px] tb:h-[286px] mo:h-[214px] flex flex-col items-center justify-center border border-blue-300">
+      <div className="mo:self-stretch tb:self-auto pc:px-13 pc:py-7 tb:px-[30px] tb:py-6 mo:px-5 mo:py-6 pc:gap-6 tb:gap-5 mo:gap-[19.47px] bg-blue-25 pc:rounded-[50px] tb:rounded-[36px] mo:rounded-[30px] tb:w-[385px] pc:h-[335px] tb:h-[287px] mo:h-[214px] flex flex-col items-center justify-center border border-blue-300">
         <div className="pc:gap-[18px] tb:gap-[14px] mo:gap-2 flex flex-col items-center justify-center">
           <div className="mo:p-[5px] tb:p-0 flex items-center justify-center">
             <div className="bg-gray-neutral-300 tb:w-[114px] tb:h-[114px] mo:w-11 mo:h-11 h-[40px] w-[40px] animate-pulse rounded-full" />
--- src/components/section/user/user-profile.tsx
+++ src/components/section/user/user-profile.tsx
@@ -7,7 +7,7 @@ const UserProfile = () => {
 
   return (
     <section className="tb:flex w-full items-center justify-center">
-      <div className="mo:self-stretch tb:self-auto pc:px-13 pc:py-7 tb:px-[30px] tb:py-6 mo:px-5 mo:py-6 pc:gap-6 tb:gap-5 mo:gap-[19.47px] bg-blue-25 pc:rounded-[50px] tb:rounded-[36px] mo:rounded-[30px] tb:w-[385px] tb:h-[350px] mo:h-[214px] flex flex-col items-center justify-center border border-blue-300">
+      <div className="mo:self-stretch tb:self-auto pc:px-13 pc:py-7 tb:px-[30px] tb:py-6 mo:px-5 mo:py-6 pc:gap-6 tb:gap-5 mo:gap-[19.47px] bg-blue-25 pc:rounded-[50px] tb:rounded-[36px] mo:rounded-[30px] tb:w-[385px] pc:h-[335px] tb:h-[287px] mo:h-[214px] flex flex-col items-center justify-center border border-blue-300">
         <div className="pc:gap-[18px] tb:gap-[14px] mo:gap-2 flex flex-col items-center justify-center">
           <div className="mo:p-[5px] tb:p-0 flex items-center justify-center">
             <Profile
